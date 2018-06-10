937bf6141766656ff3d00fc6cdaf5be5b507e613
Brian Mancini <bmancini@gmail.com>
2018-06-10 16:37:55 -0400

    Issue #1: fixing lint issue

--- packages/lightnode-invoice/src/bech32.spec.js
+++ packages/lightnode-invoice/src/bech32.spec.js
@@ -6,9 +6,6 @@ let input1 =
 let input2 =
   'lnbc2500u1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdpquwpc4curk03c9wlrswe78q4eyqc7d8d0xqzpuyk0sg5g70me25alkluzd2x62aysf2pyy8edtjeevuv4p2d5p76r4zkmneet7uvyakky2zr4cusd45tftc9c5fh0nnqpnl2jfll544esqchsrny';
 
-let input3 =
-  'lntb20m1pvjluezhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqfpp3x9et2e20v6pu37c5d9vax37wxq72un98kmzzhznpurw9sgl2v0nklu2g4d0keph5t7tj9tcqd8rexnd07ux4uv2cjvcqwaxgj7v4uwn5wmypjd5n69z2xm3xgksg28nwht7f6zspwp3f9t';
-
 test('decode should return human readable part', () => {
   let { prefix } = bech32.decode(input1);
   expect(prefix).toBe('lnbc2500u');
