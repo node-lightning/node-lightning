bce467b5b03a3b8ebd3f7be468138d3b86e97c7d
Brian Mancini <bmancini@gmail.com>
2019-02-07 22:13:03 -0500

    Adding commitment secret generation code and tests

---
+++ src/txhandling/commitment-generator.js
@@ -0,0 +1,32 @@
+const { sha256 } = require('../crypto');
+const BN = require('bn.js');
+
+module.exports = {
+  generateFromSeed,
+};
+
+// note we will need to use bignumber.js because we can't do bitmath over 2**32
+function generateFromSeed(seed, i) {
+  i = new BN(i);
+  let p = seed;
+  for (let b = 47; b >= 0; b--) {
+    if (i.and(new BN(2 ** b)).gtn(0)) {
+      // since p is a buffer...
+      //  read least significant int64 value
+      //  flip via int64 ^ 2**b
+      //  write back to buffer at least significant portion
+
+      // alternative, can calculate the byte index and flip appropriate bit inside appropriate byte
+      let byteIndex = Math.floor(b / 8);
+      let bitIndex = b % 8;
+
+      // flip the specified bit in the specific byte
+      p[byteIndex] = p[byteIndex] ^ (2 ** bitIndex);
+
+      // console.log(`flipping ${b} on byte ${byteIndex} bit ${bitIndex}`);
+
+      p = sha256(p);
+    }
+  }
+  return p;
+}
---
+++ src/txhandling/commitment-generator.spec.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai');
+const BN = require('bn.js');
+const sut = require('./commitment-generator');
+
+describe('commitmentSecretGenerator', () => {
+  describe('.generateFromSeed', () => {
+    let tests = [
+      {
+        name: '0 final node',
+        seed: '0000000000000000000000000000000000000000000000000000000000000000',
+        i: 281474976710655,
+        expected: '02a40c85b6f28da08dfdbe0926c53fab2de6d28c10301f8f7c4073d5e42e3148',
+      },
+      {
+        name: 'FF final node',
+        seed: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+        i: 281474976710655,
+        expected: '7cc854b54e3e0dcdb010d7a3fee464a9687be6e8db3be6854c475621e007a5dc',
+      },
+      {
+        name: 'FF alternate bits 1',
+        seed: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+        i: new BN('aaaaaaaaaaa', 'hex', 'be').toNumber(),
+        expected: '56f4008fb007ca9acf0e15b054d5c9fd12ee06cea347914ddbaed70d1c13a528',
+      },
+      {
+        name: 'FF alternate bits 2',
+        seed: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+        i: new BN('555555555555', 'hex', 'be').toNumber(),
+        expected: '9015daaeb06dba4ccc05b91b2f73bd54405f2be9f217fbacd3c5ac2e62327d31',
+      },
+      {
+        name: '01 last nontrivial node',
+        seed: '0101010101010101010101010101010101010101010101010101010101010101',
+        i: 1,
+        expected: '915c75942a26bb3a433a8ce2cb0427c29ec6c1775cfc78328b57f6ba7bfeaa9c',
+      },
+    ];
+
+    for (let test of tests) {
+      it(test.name, () => {
+        let actual = sut.generateFromSeed(Buffer.from(test.seed, 'hex'), test.i);
+        expect(actual).to.deep.equal(Buffer.from(test.expected, 'hex'));
+      });
+    }
+  });
+});
