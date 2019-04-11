3da53560cacc482c140d69ef2f1dc7695fdefd21
Brian Mancini <bmancini@gmail.com>
2019-04-11 19:23:37 -0400

    crypto: improving jsdoc and adding ts-check

--- packages/lntools-crypto/lib/crypto.js
+++ packages/lntools-crypto/lib/crypto.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const crypto = require('crypto');
 const secp256k1 = require('secp256k1');
 const HKDF = require('futoin-hkdf');
@@ -15,6 +17,13 @@ module.exports = {
   ccpDecrypt,
 };
 
+/**
+  Encrypts data using AES
+
+  @param {Object} opts
+  @param {Buffer} opts.key symmetric key
+  @param {Buffer} opts.buffer plaintext to encrypt
+ */
 function aesEncrypt({ key, buffer }) {
   const cipher = crypto.createCipher('aes256', key);
   let result = cipher.update(buffer);
@@ -22,6 +31,13 @@ function aesEncrypt({ key, buffer }) {
   return result;
 }
 
+/**
+  Decrypts data using AES.
+
+  @param {Object} opts
+  @param {Buffer} opts.key symmetric key
+  @param {Buffer} opts.buffer ciphertext to decrypt
+ */
 function aesDecrypt({ key, buffer }) {
   const decipher = crypto.createDecipher('aes256', key);
   let result = decipher.update(buffer);
@@ -29,6 +45,16 @@ function aesDecrypt({ key, buffer }) {
   return result;
 }
 
+/**
+  Generate a a secure key from the passphrase and salt
+  by performing PBKDF2. If no salt is provided one is
+  generated via randomBytes.
+
+  @param {Object} opts
+  @param {string} opts.passphrase
+  @param {Buffer} [opts.salt] 16-byte buffer or more. Returns
+  @returns {{ salt: Buffer, key: Buffer}}
+ */
 function generateKey({ passphrase, salt }) {
   if (!salt) {
     salt = crypto.randomBytes(16);
@@ -40,37 +66,67 @@ function generateKey({ passphrase, salt }) {
   };
 }
 
+/**
+  Hash using SHA256
+
+  @param {Buffer} data data to hash
+  @returns {Buffer} 32-byte buffer
+ */
 function sha256(data) {
   let hash = crypto.createHash('sha256');
   hash.update(data);
   return hash.digest();
 }
 
+/**
+  Hash using ripmd160
+
+  @param {Buffer} data
+  @returns {Buffer} 20-byte buffer
+ */
 function hash160(data) {
   let hash = crypto.createHash('ripemd160');
   hash.update(sha256(data));
   return hash.digest();
 }
 
+/**
+  Elliptic Curve Diffie-Helman on secp256k1 takes a
+  public key and point multiplies it by the private key.
+  The resulting compressed public key value is hashed via
+  sha256.
+
+  @param {Buffer} rk 33-byte compressed public key
+  @param {Buffer} k  32-byte private key
+  @returns {Buffer} 32-byte sha256 of the point multiple
+ */
 function ecdh(rk, k) {
-  // via secp256k1
   return secp256k1.ecdh(rk, k);
-
-  // via elliptic.js
-  // let priv = secp256k1.keyFromPrivate(k);
-  // let pub = secp256k1.keyFromPublic(rk);
-  // let shared = pub.getPublic().mul(priv.getPrivate());
-  // shared = secp256k1.keyFromPublic(shared).getPublic(true, 'hex');
-  // return sha256(Buffer.from(shared, 'hex'));
-
-  // via tiny-secp256k1
-  // return sha256(secp256k1.pointMultiply(rk, k, true));
 }
 
+/**
+  HMAC-based Extact and Expand Key Derivation Function (HKDF)
+  using SHA-256 and returning a 64-byte buffer.
+
+  @param {Buffer} salt salt, variable length
+  @param {Buffer} ikm initial key material, variable length
+  @returns {Buffer} 64-byte buffer
+ */
 function hkdf(salt, ikm) {
+  // @ts-ignore
   return HKDF(ikm, 64, { salt, hash: 'SHA-256' });
 }
 
+/**
+  Encrypt data using authenticated encryption with associated data (AEAD)
+  ChaCha20-Poly1305.
+
+  @param {Buffer} k private key, 64-bytes
+  @param {Buffer} n nonce, 12-bytes
+  @param {Buffer} ad associated data
+  @param {Buffer} plaintext raw data to encrypt
+  @returns {Buffer} encrypted data + tag as a variable length buffer
+ */
 function ccpEncrypt(k, n, ad, plaintext) {
   const cipher = chacha.createCipher(k, n);
   cipher.setAAD(ad);
@@ -82,6 +138,16 @@ function ccpEncrypt(k, n, ad, plaintext) {
   return Buffer.concat([pad, tag]);
 }
 
+/**
+  Decrypt data uusing authenticated encryption with associated data (AEAD)
+  ChaCha20-Poly1305
+
+  @param {Buffer} k private key, 64-bytes
+  @param {Buffer} n nonce, 12-bytes
+  @param {Buffer} ad associated data, variable length
+  @param {Buffer} ciphertext encrypted data to decrypt
+  @returns {Buffer} decrypteed data as a variable length Buffer
+ */
 function ccpDecrypt(k, n, ad, ciphertext) {
   const decipher = chacha.createDecipher(k, n);
   decipher.setAAD(ad);
--- packages/lntools-crypto/lib/key.js
+++ packages/lntools-crypto/lib/key.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const crypto = require('crypto');
 const secp256k1 = require('secp256k1');
 
@@ -7,6 +9,27 @@ module.exports = {
   generatePubKey,
 };
 
+/**
+  An elliptic curve key store. This class optionally contains the private
+  key and a public key in either the compressed or uncompressed format.
+
+  @typedef ECKey
+  @property {Buffer} [priv] private key Buffer, should be 32-bytes
+  @property {Buffer} pub prublic key Buffer in either compressed (33-bytes)
+    or uncompressed (64-bytes) format
+  @property {() => Buffer} compressed  Return the compressed public key for the elliptic curve key regardless
+    of whether the public key is stored as a compressed or uncompressed key.
+*/
+
+/**
+  Generates a cryptographially secure key and returns
+  an ECKey object
+
+  @remarks This method uses crypto.randomBytes to
+  generate the random value.
+
+  @returns {ECKey} the result elliptic curve key
+ */
 function generateRandomKey() {
   let privateKey = crypto.randomBytes(32);
   return {
@@ -18,6 +41,14 @@ function generateRandomKey() {
   };
 }
 
+/**
+  Generaets an ECKey object from the hex-encoded private
+  key provided.
+
+  @param {string} privKeyHex hex-encoded private key
+
+  @returns {ECKey}
+ */
 function generateKey(privKeyHex) {
   let privateKey = Buffer.from(privKeyHex, 'hex');
   return {
@@ -29,8 +60,18 @@ function generateKey(privKeyHex) {
   };
 }
 
+/**
+  Generates an ECKey object from the hex-encoded public
+  key provided.
+
+  @param {string|Buffer} compressedKey hex-encoded public key
+
+  @returns {ECKey}
+ */
 function generatePubKey(compressedKey) {
-  let publicKey = Buffer.from(compressedKey, 'hex');
+  let publicKey = Buffer.isBuffer(compressedKey)
+    ? compressedKey
+    : Buffer.from(compressedKey, 'hex');
   return {
     pub: publicKey,
     compressed() {
