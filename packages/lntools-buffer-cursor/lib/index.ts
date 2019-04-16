67433c7054fd991d9c12b7532a4e3a20e51cfc39
Brian Mancini <bmancini@gmail.com>
2019-04-16 09:03:43 -0400

    buffer-cursor: add lastReadBytes property

--- packages/lntools-buffer-cursor/lib/index.ts
+++ packages/lntools-buffer-cursor/lib/index.ts
@@ -4,11 +4,13 @@ import BN = require('bn.js');
 export class BufferCursor {
   _buffer: Buffer;
   _position: number;
+  _lastReadBytes: number;
 
   constructor(buffer: Buffer) {
     assert(Buffer.isBuffer(buffer), 'Requires a buffer');
     this._buffer = buffer;
     this._position = 0;
+    this._lastReadBytes = 0;
   }
 
   get position() {
@@ -21,6 +23,15 @@ export class BufferCursor {
 
   get buffer() {
     return this._buffer;
+  }
+
+  /**
+    Number of bytes read in last operation executed on the cursor.
+    Especially useful for operations that return variable length of
+    results such as readBytes or readVarUint.
+   */
+  get lastReadBytes() {
+    return this._lastReadBytes;
   }
 
   readUInt8() {
@@ -52,44 +63,54 @@ export class BufferCursor {
   }
 
   /**
-    Reads a variable length unsigned integer as specified
-    in the protocol documentation and aways returns a BN
+    Reads a variable length unsigned integer as specified in the protocol
+    documentation and aways returns a BN to maintain a consistant call
+    signature.
 
     @remarks
     Specified in:
     https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer
 
     Reads the first byte and determines the length of the remaining integer.
-    < 0xfd = 1 byte
-      0xfd = 2 bytes
-      0xfe = 4 bytes
-      0xff = 8 bytes
+    < 0xfd = 1 byte number
+      0xfd = 2 byte number (3 bytes total)
+      0xfe = 4 byte number (5 bytes total)
+      0xff = 8 byte number (9 bytes total)
    */
   readVarUint(): BN {
     let size = this.readUInt8();
-    if (size < 0xfd) return new BN(size);
+    if (size < 0xfd) {
+      this._lastReadBytes = 1;
+      return new BN(size);
+    }
     switch (size) {
       case 0xfd:
+        this._lastReadBytes = 3;
         return new BN(this.readUInt16LE());
       case 0xfe:
+        this._lastReadBytes = 5;
         return new BN(this.readUInt32LE());
       case 0xff:
+        this._lastReadBytes = 9;
         return this.readUInt64LE();
     }
   }
 
   readBytes(len: number) {
     if (len === 0) {
+      this._lastReadBytes = 0;
       return Buffer.alloc(0);
     } else if (len > 0) {
       if (this._position + len > this._buffer.length) throw new RangeError('Index out of range');
       let result = this._buffer.slice(this._position, this._position + len);
       this._position += len;
+      this._lastReadBytes = len;
       return result;
     } else {
       if (this._position === this._buffer.length) throw new RangeError('Index out of range');
       let result = this._buffer.slice(this._position);
       this._position = this._buffer.length;
+      this._lastReadBytes = result.length;
       return result;
     }
   }
@@ -148,6 +169,7 @@ export class BufferCursor {
   _readStandard(fn: string, len: number): number {
     let result = this._buffer[fn](this._position);
     this._position += len;
+    this._lastReadBytes = len;
     return result;
   }
 
