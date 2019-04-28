6f49f9790f7e2549aaa7d0e740b885a3c86ed2e6
Brian Mancini <bmancini@gmail.com>
2019-04-28 17:12:43 -0400

    buffer-cursor: add tests and tscheck

---
+++ packages/lntools-buffer-cursor/__tests__/index.spec.js
@@ -0,0 +1,379 @@
+const { expect } = require('chai');
+const BN = require('bn.js');
+const BufferCursor = require('../lib');
+
+const readTests = [
+  {
+    method: 'readUInt8',
+    instance: new BufferCursor(Buffer.from([1, 2, 3])),
+    expected: [1, 2, 3],
+  },
+  {
+    method: 'readUInt16LE',
+    instance: new BufferCursor(Buffer.from([1, 0, 2, 0, 3, 0])),
+    expected: [1, 2, 3],
+  },
+  {
+    method: 'readUInt16BE',
+    instance: new BufferCursor(Buffer.from([0, 1, 0, 2, 0, 3])),
+  },
+  {
+    method: 'readUInt32LE',
+    instance: new BufferCursor(Buffer.from([1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0])),
+  },
+  {
+    method: 'readUInt32BE',
+    instance: new BufferCursor(Buffer.from([0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3])),
+  },
+  {
+    method: 'readUInt64LE',
+    instance: new BufferCursor(
+      Buffer.from([1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0])
+    ),
+  },
+  {
+    method: 'readUInt64BE',
+    instance: new BufferCursor(
+      Buffer.from([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3])
+    ),
+  },
+];
+
+for (let readTest of readTests) {
+  describe(readTest.method, () => {
+    it('should read at start', () => {
+      let actual = readTest.instance[readTest.method]();
+      actual = actual instanceof BN ? actual.toNumber() : actual;
+      expect(actual).to.equal(1);
+    });
+    it('should read in middle', () => {
+      let actual = readTest.instance[readTest.method]();
+      actual = actual instanceof BN ? actual.toNumber() : actual;
+      expect(actual).to.equal(2);
+    });
+    it('should read at last', () => {
+      let actual = readTest.instance[readTest.method]();
+      actual = actual instanceof BN ? actual.toNumber() : actual;
+      expect(actual).to.equal(3);
+    });
+    it('should throw when out of bounds', () => {
+      expect(() => readTest.instance[readTest.method]()).to.throw('Index out of range');
+    });
+  });
+}
+
+describe('lastReadBytes', () => {
+  it('should start at 0', () => {
+    let sut = new BufferCursor(Buffer.from([1]));
+    expect(sut.lastReadBytes).to.equal(0);
+  });
+  it('should return for standard read', () => {
+    let sut = new BufferCursor(Buffer.from([1]));
+    sut.readUInt8();
+    expect(sut.lastReadBytes).to.equal(1);
+  });
+});
+
+describe('readBytes', () => {
+  let buffer;
+  before(() => {
+    buffer = new BufferCursor(Buffer.from([0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3]));
+  });
+  it('should read at start', () => {
+    expect(buffer.readBytes(2)).to.deep.equal(Buffer.from([0, 1]));
+  });
+  it('should read in middle', () => {
+    expect(buffer.readBytes(4)).to.deep.equal(Buffer.from([0, 0, 0, 2]));
+  });
+  it('should throw when read is out of bounds', () => {
+    expect(() => buffer.readBytes(9)).to.throw('Index out of range');
+  });
+  it('should read to end', () => {
+    expect(buffer.readBytes(8)).to.deep.equal(Buffer.from([0, 0, 0, 0, 0, 0, 0, 3]));
+  });
+  it('should throw when at end', () => {
+    expect(() => buffer.readBytes(1)).to.throw('Index out of range');
+  });
+  it('should read remaining bytes when at start', () => {
+    buffer = new BufferCursor(Buffer.from([0, 0, 0, 1]));
+    expect(buffer.readBytes()).to.deep.equal(Buffer.from([0, 0, 0, 1]));
+  });
+  it('should read remaining bytes when in middle', () => {
+    buffer = new BufferCursor(Buffer.from([0, 0, 0, 1]));
+    buffer.readBytes(1);
+    expect(buffer.readBytes()).to.deep.equal(Buffer.from([0, 0, 1]));
+  });
+  it('should throw error when at end of buffer', () => {
+    buffer = new BufferCursor(Buffer.from([1]));
+    buffer.readBytes(1);
+    expect(() => buffer.readBytes()).to.throw('Index out of range');
+  });
+  it('should return empty buffer is read length is 0', () => {
+    buffer = new BufferCursor(Buffer.alloc(0));
+    expect(buffer.readBytes(0)).to.deep.equal(Buffer.alloc(0));
+  });
+});
+
+describe('lastVarUint', () => {
+  it('should return 1 byte numbers', () => {
+    let result = new BufferCursor(Buffer.from('01', 'hex')).readVarUint();
+    expect(result.toString('hex')).to.equal('1');
+  });
+  it('should indicate 1 byte read', () => {
+    let sut = new BufferCursor(Buffer.from('01', 'hex'));
+    sut.readVarUint();
+    expect(sut.lastReadBytes).to.equal(1);
+  });
+  it('should return 2 byte numbers', () => {
+    let result = new BufferCursor(Buffer.from('fd0100', 'hex')).readVarUint();
+    expect(result.toString('hex')).to.equal('1');
+  });
+  it('should indicate 2 byte read', () => {
+    let sut = new BufferCursor(Buffer.from('fd0100', 'hex'));
+    sut.readVarUint();
+    expect(sut.lastReadBytes).to.equal(2);
+  });
+  it('should return 4 byte numbers', () => {
+    let result = new BufferCursor(Buffer.from('fe01000000', 'hex')).readVarUint();
+    expect(result.toString('hex')).to.equal('1');
+  });
+  it('should indicate 4 byte read', () => {
+    let sut = new BufferCursor(Buffer.from('fe01000000', 'hex'));
+    sut.readVarUint();
+    expect(sut.lastReadBytes).to.equal(4);
+  });
+  it('should return 8 byte numbers', () => {
+    let result = new BufferCursor(Buffer.from('ff0100000000000000', 'hex')).readVarUint();
+    expect(result.toString('hex')).to.equal('1');
+  });
+  it('should indicate 8 byte read', () => {
+    let sut = new BufferCursor(Buffer.from('ff0100000000000000', 'hex'));
+    sut.readVarUint();
+    expect(sut.lastReadBytes).to.equal(8);
+  });
+});
+
+describe('peakBytes', () => {
+  it('should return empty buffer when 0', () => {
+    let sut = new BufferCursor(Buffer.from([1]));
+    let result = sut.peakBytes(0);
+    expect(result).to.deep.equal(Buffer.alloc(0));
+  });
+  it('should return buffer of length', () => {
+    let sut = new BufferCursor(Buffer.from([1]));
+    let result = sut.peakBytes(1);
+    expect(result).to.deep.equal(Buffer.from([1]));
+  });
+  it('should return buffer of length', () => {
+    let sut = new BufferCursor(Buffer.from([1]));
+    expect(() => sut.peakBytes(2)).to.throw('Index out of range');
+  });
+  it('should return all bytes remaining when omitted', () => {
+    let sut = new BufferCursor(Buffer.from([1]));
+    let result = sut.peakBytes();
+    expect(result).to.deep.equal(Buffer.from([1]));
+  });
+  it('should throw when eof', () => {
+    let sut = new BufferCursor(Buffer.from([1]));
+    sut.readBytes();
+    expect(() => sut.peakBytes()).to.throw('Index out of range');
+  });
+});
+
+const writeTests = [
+  {
+    method: 'writeUInt8',
+    instance: new BufferCursor(Buffer.alloc(3)),
+    assertions: [Buffer.from([1, 0, 0]), Buffer.from([1, 2, 0]), Buffer.from([1, 2, 3])],
+  },
+  {
+    method: 'writeUInt16LE',
+    instance: new BufferCursor(Buffer.alloc(6)),
+    assertions: [
+      Buffer.from([1, 0, 0, 0, 0, 0]),
+      Buffer.from([1, 0, 2, 0, 0, 0]),
+      Buffer.from([1, 0, 2, 0, 3, 0]),
+    ],
+  },
+  {
+    method: 'writeUInt16BE',
+    instance: new BufferCursor(Buffer.alloc(6)),
+    assertions: [
+      Buffer.from([0, 1, 0, 0, 0, 0]),
+      Buffer.from([0, 1, 0, 2, 0, 0]),
+      Buffer.from([0, 1, 0, 2, 0, 3]),
+    ],
+  },
+  {
+    method: 'writeUInt32LE',
+    instance: new BufferCursor(Buffer.alloc(12)),
+    assertions: [
+      Buffer.from([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      Buffer.from([1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0]),
+      Buffer.from([1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0]),
+    ],
+  },
+  {
+    method: 'writeUInt32BE',
+    instance: new BufferCursor(Buffer.alloc(12)),
+    assertions: [
+      Buffer.from([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+      Buffer.from([0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0]),
+      Buffer.from([0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3]),
+    ],
+  },
+  {
+    method: 'writeUInt64LE',
+    instance: new BufferCursor(Buffer.alloc(24)),
+    assertions: [
+      Buffer.from([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      Buffer.from([1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      Buffer.from([1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0]),
+    ],
+  },
+  {
+    method: 'writeUInt64BE',
+    instance: new BufferCursor(Buffer.alloc(24)),
+    assertions: [
+      Buffer.from([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      Buffer.from([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0]),
+      Buffer.from([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3]),
+    ],
+  },
+];
+
+for (let writeTest of writeTests) {
+  describe(writeTest.method, () => {
+    it('should write at start', () => {
+      writeTest.instance[writeTest.method](1);
+      expect(writeTest.instance.buffer).to.deep.equal(writeTest.assertions[0]);
+    });
+    it('should write in middle', () => {
+      writeTest.instance[writeTest.method](2);
+      expect(writeTest.instance.buffer).to.deep.equal(writeTest.assertions[1]);
+    });
+    it('should write at end', () => {
+      writeTest.instance[writeTest.method](3);
+      expect(writeTest.instance.buffer).to.deep.equal(writeTest.assertions[2]);
+    });
+    it('should throw when out of bounds', () => {
+      expect(() => writeTest.instance[writeTest.method]()).to.throw('Index out of range');
+    });
+  });
+}
+
+describe('writeBytes', () => {
+  let buffer;
+  before(() => {
+    buffer = new BufferCursor(Buffer.alloc(8));
+  });
+  it('should ignore writing a null', () => {
+    buffer.writeBytes(null);
+    expect(buffer.buffer).to.deep.equal(Buffer.from([0, 0, 0, 0, 0, 0, 0, 0]));
+  });
+  it('should ignore writing an empty buffer', () => {
+    buffer.writeBytes(Buffer.alloc(0));
+    expect(buffer.buffer).to.deep.equal(Buffer.from([0, 0, 0, 0, 0, 0, 0, 0]));
+  });
+  it('should write at start', () => {
+    buffer.writeBytes(Buffer.from([1]));
+    expect(buffer.buffer).to.deep.equal(Buffer.from([1, 0, 0, 0, 0, 0, 0, 0]));
+  });
+  it('should write in middle', () => {
+    buffer.writeBytes(Buffer.from([2, 2]));
+    expect(buffer.buffer).to.deep.equal(Buffer.from([1, 2, 2, 0, 0, 0, 0, 0]));
+  });
+  it('should throw if write buffer exceeds cursor', () => {
+    expect(() => buffer.writeBytes(Buffer.from([6, 6, 6, 6, 6, 6]))).to.throw('Index out of range');
+  });
+  it('should write to end', () => {
+    buffer.writeBytes(Buffer.from([5, 5, 5, 5, 5]));
+    expect(buffer.buffer).to.deep.equal(Buffer.from([1, 2, 2, 5, 5, 5, 5, 5]));
+  });
+  it('should not care about writing zero byte buffer', () => {
+    buffer.writeBytes(Buffer.alloc(0));
+    expect(buffer.buffer).to.deep.equal(Buffer.from([1, 2, 2, 5, 5, 5, 5, 5]));
+  });
+});
+
+describe('position', () => {
+  let readbuf;
+  let writebuf;
+  before(() => {
+    readbuf = new BufferCursor(Buffer.from([1, 2, 0, 0, 3, 4, 0, 0, 0, 0, 0, 0, 5, 6, 0, 0, 0, 7]));
+    writebuf = new BufferCursor(Buffer.alloc(18));
+  });
+  it('should start at 0', () => {
+    expect(readbuf.position).to.equal(0);
+  });
+  it('should advance by 1 with readUInt8', () => {
+    readbuf.readUInt8();
+    expect(readbuf.position).to.equal(1);
+  });
+  it('should advance by 2 with readUInt16LE', () => {
+    readbuf.readUInt16LE();
+    expect(readbuf.position).to.equal(3);
+  });
+  it('should advance by 2 with readUInt16BE', () => {
+    readbuf.readUInt16BE();
+    expect(readbuf.position).to.equal(5);
+  });
+  it('should advance by 4 with readUInt32LE', () => {
+    readbuf.readUInt32LE();
+    expect(readbuf.position).to.equal(9);
+  });
+  it('should advance by 4 with readUInt32BE', () => {
+    readbuf.readUInt32BE();
+    expect(readbuf.position).to.equal(13);
+  });
+  it('should advance by read amount with readBytes', () => {
+    readbuf.readBytes(1);
+    expect(readbuf.position).to.equal(14);
+  });
+  it('should advance by rest with readBytes without len', () => {
+    readbuf.readBytes();
+    expect(readbuf.position).to.equal(18);
+  });
+  it('should advance by 1 with writeUInt8', () => {
+    writebuf.writeUInt8(1);
+    expect(writebuf.position).to.equal(1);
+  });
+  it('should advance by 2 with writeUInt16LE', () => {
+    writebuf.writeUInt16LE(1);
+    expect(writebuf.position).to.equal(3);
+  });
+  it('should advance by 2 with writeUInt16BE', () => {
+    writebuf.writeUInt16BE(1);
+    expect(writebuf.position).to.equal(5);
+  });
+  it('should advance by 4 with writeUInt32LE', () => {
+    writebuf.writeUInt32LE(1);
+    expect(writebuf.position).to.equal(9);
+  });
+  it('should advance by 4 with writeUInt32BE', () => {
+    writebuf.writeUInt32BE(1);
+    expect(writebuf.position).to.equal(13);
+  });
+  it('should advance by buffer length with writeBytes', () => {
+    writebuf.writeBytes(Buffer.alloc(5));
+    expect(writebuf.position).to.equal(18);
+  });
+});
+
+describe('eof', () => {
+  let buffer;
+  before(() => {
+    buffer = new BufferCursor(Buffer.from([1, 2]));
+  });
+  it('should be false at start', () => {
+    expect(buffer.eof).to.be.false;
+  });
+  it('should be false in middle', () => {
+    buffer.readBytes(1);
+    expect(buffer.eof).to.be.false;
+  });
+  it('should be true when all bytes read', () => {
+    buffer.readBytes(1);
+    expect(buffer.eof).to.be.true;
+  });
+});
--- packages/lntools-buffer-cursor/lib/index.js
+++ packages/lntools-buffer-cursor/lib/index.js
@@ -1,26 +1,48 @@
-import assert = require('assert');
-import BN = require('bn.js');
+// @ts-check
 
-export class BufferCursor {
-  _buffer: Buffer;
-  _position: number;
-  _lastReadBytes: number;
+const assert = require('assert');
+const BN = require('bn.js');
 
-  constructor(buffer: Buffer) {
+class BufferCursor {
+  /**
+    Constructs a read and write cursor for a buffer.
+    @param {Buffer} buffer
+   */
+  constructor(buffer) {
     assert(Buffer.isBuffer(buffer), 'Requires a buffer');
+
+    /** @type {Buffer} */
     this._buffer = buffer;
+
+    /** @type {number} */
     this._position = 0;
+
+    /** @type {number} */
     this._lastReadBytes = 0;
   }
 
+  /**
+    Gets the current position of the cursor in the buffer
+    @type {number}
+   */
   get position() {
     return this._position;
   }
+
+  /**
+    Gets if the cursor is at the end of file.
+    @type {boolean}
+   */
 
   get eof() {
     return this._position === this._buffer.length;
   }
 
+  /**
+    Gets the underlying buffer that the cursor
+    is reading from.
+    @type {Buffer}
+   */
   get buffer() {
     return this._buffer;
   }
@@ -34,30 +56,58 @@ export class BufferCursor {
     return this._lastReadBytes;
   }
 
+  /**
+    Read a number
+    @returns {number}
+   */
   readUInt8() {
     return this._readStandard(this.readUInt8.name, 1);
   }
 
+  /**
+    Read a number
+    @returns {number}
+   */
   readUInt16LE() {
     return this._readStandard(this.readUInt16LE.name, 2);
   }
 
+  /**
+    Read a number
+    @returns {number}
+   */
   readUInt16BE() {
     return this._readStandard(this.readUInt16BE.name, 2);
   }
 
+  /**
+    Read a number
+    @returns {number}
+   */
   readUInt32LE() {
     return this._readStandard(this.readUInt32LE.name, 4);
   }
 
+  /**
+    Read a number
+    @returns {number}
+   */
   readUInt32BE() {
     return this._readStandard(this.readUInt32BE.name, 4);
   }
 
+  /**
+    Read a number
+    @returns {BN}
+   */
   readUInt64BE() {
     return new BN(this.readBytes(8), null, 'be');
   }
 
+  /**
+    Read a number
+    @returns {BN}
+   */
   readUInt64LE() {
     return new BN(this.readBytes(8), null, 'le');
   }
@@ -76,8 +126,10 @@ export class BufferCursor {
       0xfd = 2 byte number (3 bytes total)
       0xfe = 4 byte number (5 bytes total)
       0xff = 8 byte number (9 bytes total)
+
+    @returns {BN}
    */
-  readVarUint(): BN {
+  readVarUint() {
     let size = this.readUInt8();
     if (size < 0xfd) {
       this._lastReadBytes = 1;
@@ -96,7 +148,14 @@ export class BufferCursor {
     }
   }
 
-  readBytes(len: number) {
+  /**
+    Read bytes from the buffer
+
+    @param {number} [len] optional number of bytes to read, returns
+    all remaining bytes when omitted
+    @returns {Buffer}
+   */
+  readBytes(len) {
     if (len === 0) {
       this._lastReadBytes = 0;
       return Buffer.alloc(0);
@@ -115,7 +174,14 @@ export class BufferCursor {
     }
   }
 
-  peakBytes(len: number) {
+  /**
+    Reads bytes from the buffer at the current position without
+    moving the cursor.
+
+    @param {number} len
+    @returns {Buffer}
+   */
+  peakBytes(len) {
     if (len === 0) {
       return Buffer.alloc(0);
     } else if (len > 0) {
@@ -129,36 +195,69 @@ export class BufferCursor {
     }
   }
 
-  writeUInt8(val: number) {
+  /**
+    Write at the current positiion
+    @param {number} val
+   */
+  writeUInt8(val) {
     this._writeStandard(this.writeUInt8.name, val, 1);
   }
 
-  writeUInt16LE(val: number) {
+  /**
+    Write at the current positiion
+    @param {number} val
+   */
+  writeUInt16LE(val) {
     this._writeStandard(this.writeUInt16LE.name, val, 2);
   }
 
-  writeUInt16BE(val: number) {
+  /**
+    Write at the current positiion
+    @param {number} val
+   */
+  writeUInt16BE(val) {
     this._writeStandard(this.writeUInt16BE.name, val, 2);
   }
 
-  writeUInt32LE(val: number) {
+  /**
+    Write at the current positiion
+    @param {number} val
+   */
+  writeUInt32LE(val) {
     this._writeStandard(this.writeUInt32LE.name, val, 4);
   }
 
-  writeInt32LE(val: number) {
-    this._writeStandard(this.writeInt32LE.name, val, 4);
-  }
-
-  writeUInt32BE(val: number) {
+  /**
+    Write at the current positiion
+    @param {number} val
+   */
+  writeUInt32BE(val) {
     this._writeStandard(this.writeUInt32BE.name, val, 4);
   }
 
-  writeUInt64LE(value: number | BN) {
+  /**
+    Write at the current positiion
+    @param {number|BN} value
+   */
+  writeUInt64LE(value) {
     if (!(value instanceof BN)) value = new BN(value);
     this.writeBytes(value.toBuffer('le', 8));
   }
 
-  writeBytes(buffer: Buffer) {
+  /**
+    Write at the current positiion
+    @param {number|BN} value
+   */
+  writeUInt64BE(value) {
+    if (!(value instanceof BN)) value = new BN(value);
+    this.writeBytes(value.toBuffer('be', 8));
+  }
+
+  /**
+    Write bytes at the current positiion
+    @param {Buffer} buffer
+   */
+  writeBytes(buffer) {
     if (!buffer || !buffer.length) return;
     if (this._position + buffer.length > this._buffer.length)
       throw new RangeError('Index out of range');
@@ -166,15 +265,38 @@ export class BufferCursor {
     this._position += buffer.length;
   }
 
-  _readStandard(fn: string, len: number): number {
+  /**
+    Helper for reading off buffer using built-in read functions
+    @private
+    @param {string} fn name of function
+    @param {number} len length to read
+    @returns {number}
+   */
+  _readStandard(fn, len) {
+    if (this._position + len > this._buffer.length) {
+      throw new RangeError('Index out of range');
+    }
     let result = this._buffer[fn](this._position);
     this._position += len;
     this._lastReadBytes = len;
     return result;
   }
 
-  _writeStandard(fn: string, val: number, len: number) {
+  /**
+    Helper for writing to the buffer using built-in write
+    functions
+    @private
+    @param {string} fn name of function
+    @param {number} val number to write
+    @param {number} len length of number in bytes
+   */
+  _writeStandard(fn, val, len) {
+    if (this._position + len > this._buffer.length) {
+      throw new RangeError('Index out of range');
+    }
     this._buffer[fn](val, this._position);
     this._position += len;
   }
 }
+
+module.exports = BufferCursor;
