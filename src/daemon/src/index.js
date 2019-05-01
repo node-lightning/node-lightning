c8646ae2c258ec2f03ed1fa2accfd10300753a0f
Brian Mancini <bmancini@gmail.com>
2019-05-01 07:42:25 -0400

    daemon: updating daemon exported wire

--- src/daemon/src/index.js
+++ src/daemon/src/index.js
@@ -1,7 +1,15 @@
 const winston = require('winston');
-winston.level = 'debug';
+winston.configure({
+  level: 'info',
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+});
 
-const { PeerClient } = require('@lntools/wire');
+const { Peer } = require('@lntools/wire');
 
 class App {
   static async run() {
@@ -19,27 +27,15 @@ class App {
 
   async connectToPeer() {
     // hadrcoding to demo1.lndexplorer.com
-    let pubkey = '036b96e4713c5f84dcb8030592e1bd42a2d9a43d91fa2e535b9bfd05f2c5def9b9';
+    let pubkey = Buffer.from('036b96e4713c5f84dcb8030592e1bd42a2d9a43d91fa2e535b9bfd05f2c5def9b9', 'hex'); // prettier-ignore
     let host = '38.87.54.163';
     let port = 9745;
 
     // hard coding to some testnet values for now
-    let ls = {
-      priv: Buffer.from('535e52756e85ee72c83429ce2cc0ff4c801c9d2f8f3c0fd84b2f4dac0b2c0c18', 'hex'),
-      pub: Buffer.from('02fd830096ddb75d993df04258e5bf4b17dba17c28a8ba310e3b1f9d2157c4114b', 'hex'),
-      compressed() {
-        return this.pub;
-      },
-    };
-    let rs = {
-      pub: Buffer.from(pubkey, 'hex'),
-      compressed() {
-        return this.pub;
-      },
-    };
+    let ls = Buffer.from('535e52756e85ee72c83429ce2cc0ff4c801c9d2f8f3c0fd84b2f4dac0b2c0c18', 'hex');
 
-    let peer = new PeerClient();
-    await peer.connect({ localSecret: ls, remoteSecret: rs, host, port });
+    let peer = new Peer();
+    await peer.connect({ ls, rpk: pubkey, host, port });
   }
 }
 
