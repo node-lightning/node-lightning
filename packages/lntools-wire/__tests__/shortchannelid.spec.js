7658134676558b04637b1740ac6b3c65acfb2053
Brian Mancini <bmancini@gmail.com>
2019-07-28 12:26:59 -0400

    wire: create ShortChannelId object and serialization and deserialization methods
    This commit cleans up code related to ShortChannelId to help encapsulate
    the logic for the various formats that a ShortChannelId can take on.
    
    Serialization and deserialization methods are maintained as seperate functions
    and the ShortChannelId object references the implementation functions.

--- packages/lntools-wire/__tests__/shortchannelid.spec.js
+++ packages/lntools-wire/__tests__/shortchannelid.spec.js
@@ -1,56 +1,85 @@
 const { expect } = require('chai');
+const { ShortChannelId } = require('../lib/shortchanid');
 const sut = require('../lib/shortchanid');
 
-describe('shortChannelIdBuffer', () => {
-  it('should conver the object to a buffer', () => {
-    let input = { block: 1288457, txIdx: 3, voutIdx: 0 };
+describe('ShortChannelId', () => {
+  describe('.toString', () => {
+    it('should return a human readable string', () => {
+      let input = new ShortChannelId(1288457, 3, 0);
+      let actual = input.toString();
+      expect(actual).to.equal('1288457x3x0');
+    });
+  });
+
+  describe('.toBuffer', () => {
+    it('should return a buffer with block as MSBs and voutIdx as LSBs', () => {
+      let input = new ShortChannelId(1288457, 3, 0);
+      let actual = input.toBuffer();
+      expect(actual.toString('hex')).to.deep.equal('13a9090000030000');
+    });
+  });
+
+  describe('.toNumber', () => {
+    it('should return a BN value representation of the buffer', () => {
+      let input = new ShortChannelId(1288457, 3, 0);
+      let actual = input.toNumber();
+      expect(actual.toString(10)).to.equal('1416673453389578240');
+    });
+  });
+});
+
+describe('shortChannelIdToBuffer', () => {
+  it('should convert the object to a buffer', () => {
+    let input = new ShortChannelId(1288457, 3, 0);
     let expected = Buffer.from('13a9090000030000', 'hex');
-    let result = sut.shortChannelIdBuffer(input);
+    let result = sut.shortChannelIdToBuffer(input);
     expect(result).to.deep.equal(expected);
   });
 });
 
-describe('shortChannelIdNumber', () => {
-  it('should convert the buffer to a string', () => {
-    let input = Buffer.from('13a9090000030000', 'hex');
-    let result = sut.shortChannelIdNumber(input);
-    expect(result).to.equal('1416673453389578240');
+describe('shortChannelIdToNumber', () => {
+  it('should convert the object to a human readable string', () => {
+    let input = new ShortChannelId(1288457, 3, 0);
+    let result = sut.shortChannelIdToNumber(input);
+    expect(result.toString(10)).to.equal('1416673453389578240');
   });
 });
 
-describe('shortChannelIdString', () => {
+describe('shortChannelIdToString', () => {
   it('should convert the buffer to a human readable string', () => {
-    let input = Buffer.from('083a8400034d0001', 'hex');
-    let result = sut.shortChannelIdString(input);
+    let input = new ShortChannelId(539268, 845, 1);
+    let result = sut.shortChannelIdToString(input);
     expect(result).to.equal('539268x845x1');
   });
 });
 
-describe('shortChannelIdObj', () => {
-  let expected1 = {
-    block: 539268,
-    txIdx: 845,
-    voutIdx: 1,
-  };
-
-  describe('buffer input', () => {
-    it('should convert a buffer to an object with parts', () => {
-      let input = Buffer.from('083a8400034d0001', 'hex');
-      let result = sut.shortChannelIdObj(input);
-      expect(result).to.deep.equal(expected1);
-    });
+describe('shortChannelIdFromString', () => {
+  it('should return the object', () => {
+    let input = '539268x845x1';
+    let result = sut.shortChannelIdFromString(input);
+    expect(result).to.deep.equal(new ShortChannelId(539268, 845, 1));
   });
 
-  describe('string input', () => {
-    it('should return the object', () => {
-      let input = '539268x845x1';
-      let result = sut.shortChannelIdObj(input);
-      expect(result).to.deep.equal(expected1);
-    });
+  it('should throw when not a pattern match', () => {
+    let input = '1ax2bx3c';
+    expect(() => sut.shortChannelIdFromString(input)).to.throw();
+  });
+});
 
-    it('should throw when not a pattern match', () => {
-      let input = '1ax2bx3c';
-      expect(() => sut.shortChannelIdObj(input)).to.throw();
-    });
+describe('shortChannelIdFromBuffer', () => {
+  it('should convert a buffer to an object with parts', () => {
+    let input = Buffer.from('083a8400034d0001', 'hex');
+    let result = sut.shortChannelIdFromBuffer(input);
+    expect(result).to.deep.equal(new ShortChannelId(539268, 845, 1));
+  });
+
+  it('should throw when not a buffer', () => {
+    let input = 'abc';
+    expect(() => sut.shortChannelIdFromBuffer(input)).to.throw();
+  });
+
+  it('should throw when incorrect length buffer', () => {
+    let input = Buffer.from('083a8400034d', 'hex');
+    expect(() => sut.shortChannelIdFromBuffer(input)).to.throw();
   });
 });
--- packages/lntools-wire/lib/shortchanid.js
+++ packages/lntools-wire/lib/shortchanid.js
@@ -2,19 +2,79 @@
 
 const BN = require('bn.js');
 
-module.exports = {
-  shortChannelIdBuffer,
-  shortChannelIdNumber,
-  shortChannelIdObj,
-  shortChannelIdString,
-};
+class ShortChannelId {
+  /**
+   * Defined in Lightning BOLT 07. This object represents a fast way to look
+   * up the funding transaction for a channel and consists of the block, the
+   * index of a transaction in the block, and the index of an output in that
+   * transaction.
+   *
+   * This object can take on several forms.
+   *
+   * @param {number} block
+   * @param {number} txIdx
+   * @param {number} voutIdx
+   */
+  constructor(block, txIdx, voutIdx) {
+    /**
+     * Block height
+     * @type {number}
+     */
+    this.block = block;
+
+    /**
+     * Transaction index in the block
+     * @type {number}
+     */
+    this.txIdx = txIdx;
+
+    /**
+     * Output index in transaction
+     * @type {number}
+     */
+    this.voutIdx = voutIdx;
+  }
+
+  /**
+   * Converts the short_channel_id into a number
+   * @returns {BN}
+   */
+  toNumber() {
+    return shortChannelIdToNumber(this);
+  }
+
+  /**
+   * Converts the short_channel_id into a human
+   * readable string in the format [block]x[txidx]x[voutidx]
+   * @returns {string}
+   */
+  toString() {
+    return shortChannelIdToString(this);
+  }
+
+  /**
+   * Converts the short_chanenl_id into a buffer in the expected
+   * serialization format.
+   * @returns {Buffer}
+   */
+  toBuffer() {
+    return shortChannelIdToBuffer(this);
+  }
+}
+
+exports.ShortChannelId = ShortChannelId;
+exports.shortChannelIdToBuffer = shortChannelIdToBuffer;
+exports.shortChannelIdToNumber = shortChannelIdToNumber;
+exports.shortChannelIdToString = shortChannelIdToString;
+exports.shortChannelIdFromString = shortChannelIdFromString;
+exports.shortChannelIdFromBuffer = shortChannelIdFromBuffer;
 
 /**
  * Converts the shortChannelId object into a buffer
- * @param {{ block: number, txIdx: number, voutIdx: number }} obj
+ * @param {ShortChannelId} obj
  * @returns {Buffer}
  */
-function shortChannelIdBuffer(obj) {
+function shortChannelIdToBuffer(obj) {
   if (!obj) return Buffer.alloc(8);
   let buf = Buffer.alloc(8);
   buf.writeUIntBE(obj.block, 0, 3);
@@ -24,63 +84,23 @@ function shortChannelIdBuffer(obj) {
 }
 
 /**
-  Returns the shortChannelId as a base 10 number string.
-  @param {Buffer} buf
-  @returns {string}
+ * Returns the short_channel_id as a BigNumber object that can be
+ * easily converted into a base10 string.
+ * @param {ShortChannelId} obj
+ * @returns {BN}
  */
-function shortChannelIdNumber(buf) {
-  return new BN(buf).toString(10);
+function shortChannelIdToNumber(obj) {
+  let buf = shortChannelIdToBuffer(obj);
+  return new BN(buf);
 }
 
 /**
- * Returns the shortChannelId as an object containing
- * the block number, transaction index and vout index. The string
- * input should be the shortChannelId string format.
- * @param {Buffer|string} arg
- * @returns {{ block: number, txIdx: number, voutIdx: number }}
- */
-function shortChannelIdObj(arg) {
-  if (Buffer.isBuffer(arg)) {
-    return _objectFromBuffer(arg);
-  } else if (typeof arg === 'string') {
-    return _objectFromString(arg);
-  } else {
-    throw new Error('Invalid argument type');
-  }
-}
-
-/**
-  Returns the shortChannelId as a readable string in the format
-  "<block>x<tx_index>x<vout_index>"
-  @param {Buffer} buf
-  @returns {string} human readable string
- */
-function shortChannelIdString(buf) {
-  let { block, txIdx, voutIdx } = shortChannelIdObj(buf);
-  return `${block}x${txIdx}x${voutIdx}`;
-}
-
-///////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {Buffer} buf
- * @returns {{ block: number, txIdx: number, voutIdx: number }}
- */
-function _objectFromBuffer(buf) {
-  return {
-    block: buf.readUIntBE(0, 3),
-    txIdx: buf.readUIntBE(3, 3),
-    voutIdx: buf.readUIntBE(6, 2),
-  };
-}
-
-/**
- * @private
+ * Creates a ShortChannelId from a string matching the pattern
+ * [block]x[txIdx]x[voutIdx].
  * @param {string} text
- * @returns {{ block:number, txIdx:number, voutIdx: number }}
+ * @returns {ShortChannelId}
  */
-function _objectFromString(text) {
+function shortChannelIdFromString(text) {
   let match = text.match(/(\d+?)x(\d+?)x(\d+?)/);
   if (!match) {
     throw new Error('invalid shortChannelId string');
@@ -88,9 +108,31 @@ function _objectFromString(text) {
   let block = parseInt(match[1]);
   let txIdx = parseInt(match[2]);
   let voutIdx = parseInt(match[3]);
-  return {
-    block,
-    txIdx,
-    voutIdx,
-  };
+  return new ShortChannelId(block, txIdx, voutIdx);
+}
+
+/**
+ * Creates a ShortChannelId from a buffer.
+ * @param {Buffer} buf
+ * @returns {ShortChannelId}
+ */
+function shortChannelIdFromBuffer(buf) {
+  if (!Buffer.isBuffer(buf) || buf.length !== 8) {
+    throw new Error('invalid argument');
+  }
+  let block = buf.readUIntBE(0, 3);
+  let txIdx = buf.readUIntBE(3, 3);
+  let voutIdx = buf.readUIntBE(6, 2);
+  return new ShortChannelId(block, txIdx, voutIdx);
+}
+
+/**
+  Returns the shortChannelId as a readable string in the format
+  "<block>x<tx_index>x<vout_index>"
+  @param {ShortChannelId} obj
+  @returns {string} human readable string
+ */
+function shortChannelIdToString(obj) {
+  let { block, txIdx, voutIdx } = obj;
+  return `${block}x${txIdx}x${voutIdx}`;
 }
