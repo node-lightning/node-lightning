2734475108614b43eda844b40fdf7cbf89d7bd8f
Brian Mancini <bmancini@gmail.com>
2020-02-03 17:55:26 -0500

    logger: release 0.5.0

--- packages/lntools-logger/__tests__/logger.spec.ts
+++ packages/lntools-logger/__tests__/logger.spec.ts
@@ -1,8 +1,8 @@
 // tslint:disable: no-unused-expression
 import { expect } from "chai";
 import sinon from "sinon";
-import { Logger } from "../lib/logger";
 import { LogLevel } from "../lib/log-level";
+import { Logger } from "../lib/logger";
 import { ITransport } from "../lib/transport";
 
 describe("Logger", () => {
