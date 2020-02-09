145d8f8c38f6b523554e0038c375ea3a8992c4ef
Brian Mancini <bmancini@gmail.com>
2020-02-09 18:31:39 -0500

    crypto: native support for chacha20-poly1305
    This commit removes the dependency on chacha-native for the OpenSSL
    ChaCha20-Poly1305 AEAD support that was included in Node 11.x. This
    change also fixes issues with upgrading to Node 12 that were
    caused by the chacha-native module.

--- packages/lntools-crypto/lib/chachapoly.ts
+++ packages/lntools-crypto/lib/chachapoly.ts
@@ -1,4 +1,4 @@
-import chacha from "chacha-native";
+import crypto from "crypto";
 
 /**
  * Encrypt data using authenticated encryption with associated data (AEAD)
@@ -11,14 +11,11 @@ import chacha from "chacha-native";
  * @returns encrypted data + tag as a variable length buffer
  */
 export function ccpEncrypt(k: Buffer, n: Buffer, ad: Buffer, plaintext: Buffer): Buffer {
-  const cipher = chacha.createCipher(k, n);
-  cipher.setAAD(ad);
-  // @ts-ignore
+  const cipher = crypto.createCipheriv("ChaCha20-Poly1305" as any, k, n, { authTagLength: 16 });
+  cipher.setAAD(ad, undefined);
   const pad = cipher.update(plaintext);
-  // @ts-ignore
   cipher.final();
   const tag = cipher.getAuthTag();
-
   return Buffer.concat([pad, tag]);
 }
 
@@ -33,21 +30,18 @@ export function ccpEncrypt(k: Buffer, n: Buffer, ad: Buffer, plaintext: Buffer):
  * @returns decrypteed data as a variable length Buffer
  */
 export function ccpDecrypt(k: Buffer, n: Buffer, ad: Buffer, ciphertext: Buffer) {
-  const decipher = chacha.createDecipher(k, n);
-  decipher.setAAD(ad);
+  const decipher = crypto.createDecipheriv("ChaCha20-Poly1305" as any, k, n, { authTagLength: 16 });
+  decipher.setAAD(ad, undefined);
 
   if (ciphertext.length === 16) {
     decipher.setAuthTag(ciphertext);
-    // @ts-ignore
     return decipher.final();
   }
   if (ciphertext.length > 16) {
     const tag = ciphertext.slice(ciphertext.length - 16);
     const pad = ciphertext.slice(0, ciphertext.length - 16);
     decipher.setAuthTag(tag);
-    // @ts-ignore
     let m = decipher.update(pad);
-    // @ts-ignore
     const f = decipher.final();
     m = Buffer.concat([m, f]);
     return m;
