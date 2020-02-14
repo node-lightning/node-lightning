5abc182ca9f4ba5a8fc17752501d9a5f38680401
Brian Mancini <bmancini@gmail.com>
2020-02-14 12:15:47 -0500

    crypto: implements RFC 5869 compliant HKDF
    Simple implementation of a HKDF using the native node HMAC class.
    This class performs the extract and expand as a single step. It
    confirms to the test vectors in RFC 5869 and BOLT #8.

--- packages/lntools-crypto/lib/hkdf.ts
+++ packages/lntools-crypto/lib/hkdf.ts
@@ -1,24 +1,42 @@
-import HKDF from "futoin-hkdf";
+import crypto from "crypto";
 
 /**
  * HMAC-based Extact and Expand Key Derivation Function (HKDF)
- * using SHA-256 and returning a 64-byte buffer.
+ * that complies with RFC 5869
+ *
+ * @remarks
+ * Refer to https://tools.ietf.org/html/rfc5869 for detailed information
  *
  * @param ikm initial key material, variable length
  * @param len output key material length
  * @param salt optional, defaults to none
  * @param info optional, defaults to none
  * @param hash optional, defaults to sha256
- * @param salt salt, variable length
- * @returns 64-byte buffer
+ * @returns output key material of specified length
  */
 export function hkdf(
   ikm: Buffer,
-  len: number = 64,
+  len: number,
   salt: Buffer = Buffer.alloc(0),
   info: Buffer = Buffer.alloc(0),
   hash: string = "sha256",
-): Buffer {
-  // @ts-ignore
-  return HKDF(ikm, len, { salt, hash, info });
+) {
+  // extract step
+  const prk = hmacHash(salt, ikm, hash);
+
+  // expand
+  const n = Math.ceil(len / prk.byteLength);
+  const t = [Buffer.alloc(0)];
+  for (let i = 1; i <= n; i++) {
+    const tp = t[t.length - 1];
+    const bi = Buffer.from([i]);
+    t.push(hmacHash(prk, Buffer.concat([tp, info, bi]), hash));
+  }
+  return Buffer.concat(t.slice(1)).slice(0, len);
+}
+
+function hmacHash(key: Buffer, input: Buffer, hash: string) {
+  const hmac = crypto.createHmac(hash, key);
+  hmac.update(input);
+  return hmac.digest();
 }
