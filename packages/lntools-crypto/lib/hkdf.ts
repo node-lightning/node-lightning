f6e48fbbedf4e5bf1cf33529551caf06d9f3f9b7
Brian Mancini <bmancini@gmail.com>
2020-02-14 12:11:00 -0500

    crypto: adding HKDF tests for RFC 5869 and BOLT #8
    This change adds tests for RFC 5869 and BOLT #8 for HKDF. This is a
    breaking change to support the features needed for these test vectors.

--- packages/lntools-crypto/lib/hkdf.ts
+++ packages/lntools-crypto/lib/hkdf.ts
@@ -4,11 +4,21 @@ import HKDF from "futoin-hkdf";
  * HMAC-based Extact and Expand Key Derivation Function (HKDF)
  * using SHA-256 and returning a 64-byte buffer.
  *
- * @param salt salt, variable length
  * @param ikm initial key material, variable length
+ * @param len output key material length
+ * @param salt optional, defaults to none
+ * @param info optional, defaults to none
+ * @param hash optional, defaults to sha256
+ * @param salt salt, variable length
  * @returns 64-byte buffer
  */
-export function hkdf(salt: Buffer, ikm: Buffer): Buffer {
+export function hkdf(
+  ikm: Buffer,
+  len: number = 64,
+  salt: Buffer = Buffer.alloc(0),
+  info: Buffer = Buffer.alloc(0),
+  hash: string = "sha256",
+): Buffer {
   // @ts-ignore
-  return HKDF(ikm, 64, { salt, hash: "SHA-256" });
+  return HKDF(ikm, len, { salt, hash, info });
 }
