3987a1745794cb064808bf816cc007a20b32ad55
Brian Mancini <bmancini@gmail.com>
2020-03-05 16:09:56 -0500

    wire: converting remaining tests to typescript

--- packages/lntools-wire/__tests__/pingpong-state.spec.ts
+++ packages/lntools-wire/__tests__/pingpong-state.spec.ts
@@ -1,13 +1,13 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
-const { PingPongState } = require('../lib/pingpong-state');
+import { expect } from "chai";
+import sinon from "sinon";
+import { PingPongState } from "../lib/pingpong-state";
 
-function createAndStart({ PING_INTERVAL_MS, PONG_TIMEOUT_MS, PING_FLOOD_THRESHOLD }) {
-  let peerClient = {
+function createAndStart({ PING_INTERVAL_MS, PONG_TIMEOUT_MS, PING_FLOOD_THRESHOLD }: any) {
+  const peerClient = {
     disconnect: sinon.stub(),
     sendMessage: sinon.stub(),
   };
-  let sut = new PingPongState(peerClient);
+  const sut = new PingPongState(peerClient as any);
   if (PING_INTERVAL_MS) {
     sut.PING_INTERVAL_MS = PING_INTERVAL_MS;
   }
@@ -25,16 +25,16 @@ function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-describe('pingpong-state', () => {
+describe("pingpong-state", () => {
   let sut;
 
   afterEach(() => {
     sut.onDisconnecting();
   });
 
-  describe('when ping interval expires', () => {
-    describe('when no message has been received', () => {
-      it('it should send a ping', async () => {
+  describe("when ping interval expires", () => {
+    describe("when no message has been received", () => {
+      it("it should send a ping", async () => {
         sut = createAndStart({ PING_INTERVAL_MS: 10 });
         await wait(15);
         expect(sut._peerClient.sendMessage.args[0][0]).to.deep.equal({
@@ -44,8 +44,8 @@ describe('pingpong-state', () => {
         });
       });
     });
-    describe('when last message is old', () => {
-      it('it should send a ping', async () => {
+    describe("when last message is old", () => {
+      it("it should send a ping", async () => {
         sut = createAndStart({ PING_INTERVAL_MS: 10 });
         await wait(15);
         sut.onMessage({ type: 258 });
@@ -56,8 +56,8 @@ describe('pingpong-state', () => {
         });
       });
     });
-    describe('when message has been received', () => {
-      it('it should not send ping', async () => {
+    describe("when message has been received", () => {
+      it("it should not send ping", async () => {
         sut = createAndStart({ PING_INTERVAL_MS: 10 });
         await wait(5);
         sut.onMessage({ type: 258 });
@@ -67,29 +67,29 @@ describe('pingpong-state', () => {
     });
   });
 
-  describe('when pong expires', () => {
-    it('it should disconnect', async () => {
+  describe("when pong expires", () => {
+    it("it should disconnect", async () => {
       sut = createAndStart({ PING_INTERVAL_MS: 10, PONG_TIMEOUT_MS: 1 });
       await wait(15);
       expect(sut._peerClient.disconnect.callCount).to.equal(1);
     });
   });
 
-  describe('when pong received', () => {
-    it('it should cancel pong timeout', async () => {
+  describe("when pong received", () => {
+    it("it should cancel pong timeout", async () => {
       sut = createAndStart({ PING_INTERVAL_MS: 10, PONG_TIMEOUT_MS: 3 });
       await wait(12);
       sut.onMessage({ type: 19, ignored: Buffer.alloc(1) });
       await wait(5);
       expect(sut._peerClient.disconnect.callCount).to.equal(0);
     });
-    it('it should be happy if valid pong', async () => {
+    it("it should be happy if valid pong", async () => {
       sut = createAndStart({ PING_INTERVAL_MS: 10, PONG_TIMEOUT_MS: 3 });
       await wait(12);
       sut.onMessage({ type: 19, ignored: Buffer.alloc(1) });
       expect(sut._peerClient.disconnect.callCount).to.equal(0);
     });
-    it('it should disconnect if invalid pong', async () => {
+    it("it should disconnect if invalid pong", async () => {
       sut = createAndStart({ PING_INTERVAL_MS: 10, PONG_TIMEOUT_MS: 3 });
       await wait(12);
       sut.onMessage({ type: 19, ignored: Buffer.alloc(2) });
@@ -97,13 +97,13 @@ describe('pingpong-state', () => {
     });
   });
 
-  describe('when ping received', () => {
-    it('it should not send ping when numPongBytes gte 65532', () => {
+  describe("when ping received", () => {
+    it("it should not send ping when numPongBytes gte 65532", () => {
       sut = createAndStart({ PING_INTERVAL_MS: 10, PONG_TIMEOUT_MS: 3 });
       sut.onMessage({ type: 18, numPongBytes: 65532 });
       expect(sut._peerClient.disconnect.callCount).to.equal(0);
     });
-    it('it should send correct pong', () => {
+    it("it should send correct pong", () => {
       sut = createAndStart({ PING_INTERVAL_MS: 10, PONG_TIMEOUT_MS: 3 });
       sut.onMessage({ type: 18, numPongBytes: 1 });
       expect(sut._peerClient.sendMessage.args[0][0]).to.deep.equal({
@@ -111,7 +111,7 @@ describe('pingpong-state', () => {
         ignored: Buffer.alloc(1),
       });
     });
-    it('it should disconnect if ping flood', () => {
+    it("it should disconnect if ping flood", () => {
       sut = createAndStart({ PING_FLOOD_THRESHOLD: 2 });
       sut.onMessage({ type: 18, numPongBytes: 1 });
       sut.onMessage({ type: 18, numPongBytes: 1 });
@@ -120,8 +120,8 @@ describe('pingpong-state', () => {
     });
   });
 
-  describe('normal ping frequency', () => {
-    it('should not trigger disconnect', async () => {
+  describe("normal ping frequency", () => {
+    it("should not trigger disconnect", async () => {
       sut = createAndStart({ PING_FLOOD_THRESHOLD: 2, PING_INTERVAL_MS: 10 });
       sut.onMessage({ type: 18, numPongBytes: 1 });
       await wait(10);
