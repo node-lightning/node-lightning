085b34eb8c4ee5710c46662c3dce43ac44e263da
Brian Mancini <bmancini@gmail.com>
2020-03-30 13:34:32 -0400

    wire: adding tests for ext_chan_ann capacity tlv

---
+++ packages/lntools-wire/__tests__/messages/tlv/extended-channel-announcement-capacity.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import { ExtendedChannelAnnouncementCapacity } from "../../../lib/messages/tlvs/extended-channel-announcement-capacity";
+import { TlvValueReader } from "../../../lib/serialize/tlv-value-reader";
+
+describe("ExtendedChannelAnnouncementCapacity", () => {
+  describe(".serializeValue()", () => {
+    it("should create buffer", () => {
+      const sut = new ExtendedChannelAnnouncementCapacity();
+      sut.capacity = BigInt(1234);
+      const actual = sut.serializeValue();
+      expect(actual.toString("hex")).to.equal("04d2");
+    });
+  });
+
+  describe(".serialize()", () => {
+    it("should create full TLV buffer", () => {
+      const sut = new ExtendedChannelAnnouncementCapacity();
+      sut.capacity = BigInt(1234);
+      const actual = sut.serialize();
+      expect(actual.toString("hex")).to.equal(
+        "fe01000039" + // type (16777273)
+        "02" + // length (2)
+        "04d2", // value
+      ); // prettier-ignore
+    });
+  });
+
+  describe(".deserialize", () => {
+    it("should create instance", () => {
+      const payload = Buffer.from("04d2", "hex");
+      const reader = new TlvValueReader(payload);
+      const actual = ExtendedChannelAnnouncementCapacity.deserialize(reader);
+      expect(actual.capacity).to.equal(BigInt(1234));
+    });
+  });
+});
