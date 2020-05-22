085f05159479b75c6626820e5471f0f518012338
Brian Mancini <bmancini@gmail.com>
2020-05-22 15:25:16 -0400

    gossip: creates the PeerGossipReceiver class
    This class controls the logic for use with gossip_queries and gossip_queries_ex
    and contains the logic for sending the gossip_timestamp_filter message.
    
    This code is being removed from the PeerGossipSynchronizer in a refactor that
    will contain numerous strategies for performing gossip synchronization.

---
+++ packages/lntools-wire/__tests__/gossip/peer-gossip-receiver.spec.ts
@@ -0,0 +1,96 @@
+// tslint:disable: no-unused-expression
+
+import { ILogger } from "@lntools/logger";
+import { expect } from "chai";
+import sinon from "sinon";
+import { PeerGossipReceiver } from "../../lib/gossip/peer-gossip-receiver";
+import { PeerGossipSynchronizer } from "../../lib/gossip/peer-gossip-synchronizer";
+import { GossipTimestampFilterMessage } from "../../lib/messages/gossip-timestamp-filter-message";
+import { QueryChannelRangeMessage } from "../../lib/messages/query-channel-range-message";
+import { QueryShortChannelIdsMessage } from "../../lib/messages/query-short-channel-ids-message";
+import { ReplyChannelRangeMessage } from "../../lib/messages/reply-channel-range-message";
+import { ReplyShortChannelIdsEndMessage } from "../../lib/messages/reply-short-channel-ids-end-message";
+import { IWireMessage } from "../../lib/messages/wire-message";
+import { ShortChannelId } from "../../lib/shortchanid";
+import { createFakeLogger, createFakePeer } from "../_test-utils";
+import { PeerGossipReceiveState } from "../../lib/gossip/peer-gossip-receive-state";
+
+describe("PeerGossipSynchronizer", () => {
+  let chainHash: Buffer;
+  let sut: PeerGossipReceiver;
+  let peer: any;
+  let logger: ILogger;
+
+  beforeEach(() => {
+    chainHash = Buffer.alloc(32, 1);
+    peer = createFakePeer();
+    logger = createFakeLogger();
+    sut = new PeerGossipReceiver(chainHash, peer, logger);
+  });
+
+  describe("initial state", () => {
+    it("in 'inactive' state", () => {
+      expect(sut.receiveState).to.equal(PeerGossipReceiveState.Inactive);
+    });
+
+    it("has uint32_max start", () => {
+      expect(sut.firstTimestamp).to.equal(4294967295);
+    });
+
+    it("has zero range", () => {
+      expect(sut.timestampRange).to.equal(0);
+    });
+  });
+
+  describe(".activate()", () => {
+    it("should send gossip_timestamp_filter", () => {
+      sut.activate(1590174550, 4294967294);
+      expect(peer.sendMessage.callCount).to.equal(1);
+
+      const msg: GossipTimestampFilterMessage = peer.sendMessage.args[0][0];
+      expect(msg.firstTimestamp).to.equal(1590174550);
+      expect(msg.timestampRange).to.equal(4294967294);
+    });
+
+    it("should transition state to 'receiving'", () => {
+      sut.activate(1590174550, 4294967294);
+      expect(sut.receiveState).to.equal(PeerGossipReceiveState.Receiving);
+    });
+
+    it("sets timestamp", () => {
+      sut.activate(1590174550, 4294967294);
+      expect(sut.firstTimestamp).to.equal(1590174550);
+    });
+
+    it("sets range", () => {
+      sut.activate(1590174550, 4294967294);
+      expect(sut.timestampRange).to.equal(4294967294);
+    });
+  });
+
+  describe(".deactivate()", () => {
+    it("should send gossip_timestamp_filter", () => {
+      sut.deactivate();
+      expect(peer.sendMessage.callCount).to.equal(1);
+
+      const msg: GossipTimestampFilterMessage = peer.sendMessage.args[0][0];
+      expect(msg.firstTimestamp).to.equal(4294967295);
+      expect(msg.timestampRange).to.equal(0);
+    });
+
+    it("should transition state to 'inactive'", () => {
+      sut.deactivate();
+      expect(sut.receiveState).to.equal(PeerGossipReceiveState.Inactive);
+    });
+
+    it("sets timestamp", () => {
+      sut.deactivate();
+      expect(sut.firstTimestamp).to.equal(4294967295);
+    });
+
+    it("sets range", () => {
+      sut.deactivate();
+      expect(sut.timestampRange).to.equal(0);
+    });
+  });
+});
