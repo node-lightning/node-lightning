485944e86efcc3ac1b0274a6afc9581c60f4d2f7
Brian Mancini <bmancini@gmail.com>
2020-06-12 12:40:26 -0400

    crypto: adding secp256k1 methods
    Adds scalar multiply, point addition, private key mult, private key add
    operations.

--- packages/crypto/lib/secp256k1.ts
+++ packages/crypto/lib/secp256k1.ts
@@ -26,3 +26,47 @@ export function ecdh(rk: Buffer, k: Buffer): Buffer {
 export function verifySig(msg: Buffer, sig: Buffer, pubkey: Buffer): boolean {
     return secp256k1.verify(msg, sig, pubkey);
 }
+
+/**
+ * Adds the points together to get a new point
+ * @param points public keys that are either compressed or uncompressed
+ * @param compressed indicates if the points are compressed or uncompressed
+ * @returns a new point as either 33-byte compressed public key or 64-byte
+ * uncompresses public key
+ */
+export function pointAdd(points: Buffer[], compressed: boolean = true): Buffer {
+    return secp256k1.publicKeyCombine(points, compressed);
+}
+
+/**
+ * Scalar multiplies a point by the tweak amount
+ * @param point public key in either compressed or uncompressed format
+ * @param tweak a 32-byte value that scalar multiplies by the value
+ * @param compressed
+ * @returns a new point as either 33-byte compressed public key or 64-byte
+ * uncompressed public key
+ */
+export function scalarMul(point: Buffer, tweak: Buffer, compressed: boolean = true): Buffer {
+    return secp256k1.publicKeyTweakMul(point, tweak, compressed);
+}
+
+/**
+ * Performs modular addition on the secret value e by adding the tweak value
+ * @param secret 32-byte secret
+ * @param tweak 32-byte tweak
+ * @returns 32-byte secret
+ */
+export function privateKeyAdd(secret: Buffer, tweak: Buffer): Buffer {
+    return secp256k1.privateKeyTweakAdd(secret, tweak);
+}
+
+/**
+ * Performs modular multiplication on the secret value e by multiplying by the
+ * tweak value
+ * @param secret 32-byte secret
+ * @param tweak 32-byte tweak
+ * @returns 32-byte secret
+ */
+export function privateKeyMul(secret: Buffer, tweak: Buffer): Buffer {
+    return secp256k1.privateKeyTweakMul(secret, tweak);
+}
