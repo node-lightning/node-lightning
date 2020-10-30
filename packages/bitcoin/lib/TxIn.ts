d55434d7e2ce77b97bdef632f84407c2da8cfdfc
bmancini55 <bmancini@gmail.com>
2020-10-30 11:51:20 -0400

    bitcoin: add TxIn type
    This type represents a transaction input and includes codec methods for
    a TxIn value.

---
+++ packages/bitcoin/lib/TxIn.ts
@@ -0,0 +1,97 @@
+import { BufferWriter, StreamReader } from "@node-lightning/bufio";
+import { HashValue } from "./HashValue";
+import { Script } from "./Script";
+import { ScriptCmd } from "./ScriptCmd";
+
+export class TxIn {
+    /**
+     * Parses a TxIn from a a stream reader.
+     * @param stream
+     */
+    public static parse(reader: StreamReader): TxIn {
+        const prevTxId = HashValue.parse(reader);
+        const prevTxIndex = reader.readUInt32LE();
+        const scriptSig = Script.parse(reader);
+        const sequence = reader.readUInt32LE();
+        return new TxIn(prevTxId, prevTxIndex, scriptSig, sequence);
+    }
+
+    /**
+     * The identifier for the previous transaction that the input spends
+     * from.
+     */
+    public prevTxId: HashValue;
+
+    /**
+     * The index of the previous transaction that the input spends from.
+     */
+    public prevTxIndex: number;
+
+    /**
+     * ScriptSig for the input
+     */
+    public scriptSig: Script;
+
+    /**
+     * nSequence value for the transaction. Defaults to 0xffffffff which
+     * disables the absolute timelock.
+     */
+    public sequence: number;
+
+    /**
+     * Witness data that is required by the input
+     */
+    public witness: ScriptCmd[];
+
+    /**
+     * Constructs a new transaction input from the values
+     * @param prevTxId
+     * @param prevTxIndex
+     * @param scriptSig
+     * @param sequence
+     */
+    constructor(
+        prevTxId: HashValue,
+        prevTxIndex: number,
+        scriptSig: Script = new Script(),
+        sequence: number = 0xffffffff,
+    ) {
+        this.prevTxId = prevTxId;
+        this.prevTxIndex = prevTxIndex;
+        this.scriptSig = scriptSig;
+        this.sequence = sequence;
+    }
+
+    /**
+     * Creates a string of the transaction input that includes all of the
+     * properties.
+     */
+    public toString() {
+        return `prev=${this.prevTxId.toString()}, prevIdx=${this.prevTxIndex}, scriptSig=${this.scriptSig.toString()}, sequence=${this.sequence}`; // prettier-ignore
+    }
+
+    /**
+     * Creates a JSON object of the transaction input that includes all
+     * of the properties.
+     */
+    public toJSON() {
+        return {
+            prevTxId: this.prevTxId.toJSON(),
+            prevTxIndex: this.prevTxIndex,
+            scriptSig: this.scriptSig.toJSON(),
+            sequence: this.sequence,
+        };
+    }
+
+    /**
+     * Returns the byte serialization of the transaction input
+     */
+    public serialize(): Buffer {
+        const writer = new BufferWriter();
+        writer.writeBytes(this.prevTxId.serialize());
+        writer.writeUInt32LE(this.prevTxIndex);
+        writer.writeBytes(this.scriptSig.serialize());
+        writer.writeUInt32LE(this.sequence);
+        return writer.toBuffer();
+    }
+}
