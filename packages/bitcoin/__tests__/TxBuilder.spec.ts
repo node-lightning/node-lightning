7870d806940e4bba4a595017dae3e7be154d7401
bmancini55 <bmancini@gmail.com>
2021-01-29 15:32:28 -0500

    bitcoin: add p2ms to TxBuilder tests
    Adds test coverage for sending and spending p2ms transactions.

--- packages/bitcoin/__tests__/TxBuilder.spec.ts
+++ packages/bitcoin/__tests__/TxBuilder.spec.ts
@@ -1,43 +1,22 @@
-import { StreamReader } from "@node-lightning/bufio";
 import { getPublicKey, hash160 } from "@node-lightning/crypto";
 import { expect } from "chai";
-import { time } from "console";
 import { OpCode } from "../lib/OpCodes";
 import { OutPoint } from "../lib/OutPoint";
 import { Script } from "../lib/Script";
-import { Tx } from "../lib/Tx";
 import { TxBuilder } from "../lib/TxBuilder";
-import { TxIn } from "../lib/TxIn";
-import { TxInSequence } from "../lib/TxInSequence";
-import { TxLockTime } from "../lib/TxLockTime";
-import { TxOut } from "../lib/TxOut";
 import { Value } from "../lib/Value";
 
-// 1:
-// address: mufrrGX5ei1g2GBjKBBYvidioNqN7GWJsD
-// pkh:     9b40f5b05efd99e4b0c4f62ca63eec3e580e95c7
-// wif:     cSV9NvF6Gk3gTXgrpfPrjkHCyb4CLQZD3nsQSYUKGH4PPsRS8J2M
-// priv:    92541d4cdb6f0db05949e4fd91d7cb936f84eb21a62b477103d0e1957e6ad782
-
-// 2:
-// address: myVmRHXHq6qzW7s7TCseBLLgajKxT6Z19j
-// pkh:     c538c517797dfefdf30142dc1684bfd947532dbb
-// wif:     cSAyZjTwLx2eh76MfL5zmGV8n7pk61Lx5We2S95UGyhiwoSgJgv2
-// priv:    88fb4d47adfd310bf071d67c8ac569f58d8e4c4d8d94b778c010ddd0e2ff6a48
-
-//
-// bitcoin-cli -regtest getnewaddress addr2 legacy
-// bitcoin-cli -regtest dumpprivkey mufrrGX5ei1g2GBjKBBYvidioNqN7GWJsD
-// bitcoin-cli -regtest generatetoaddress 101 mufrrGX5ei1g2GBjKBBYvidioNqN7GWJsD
-// bitcoin-cli -regtest generatetoaddress 1 mufrrGX5ei1g2GBjKBBYvidioNqN7GWJsD
-// bitcoin-cli -regtest sendrawtransaction 020000000168de699575d42639235114a0b9b43a6ed3317e72b60601ad9f0a0273ad630e9d000000006b4830450221009fdc678141bfad627023ae38336716543f91c1b12462673bc9d4fddd6cba5f3c02202e8dd9c9ce538c6bc9b29612ccbf07b3051c13e0f40e92ce3e663060da4c803a012102c13bf903d6147a7fec59b450e2e8a6c174c35a11a7675570d10bd05bc3597996ffffffff01f0ca052a010000001976a914c538c517797dfefdf30142dc1684bfd947532dbb88acffffffff
-// 4fc69683b7f02a4750cad1847d21d469f4189acac2af24042d1f9356b8ef2181
-
 describe("TxBuilder", () => {
+    // address: mufrrGX5ei1g2GBjKBBYvidioNqN7GWJsD
+    // pkh:     9b40f5b05efd99e4b0c4f62ca63eec3e580e95c7
+    // wif:     cSV9NvF6Gk3gTXgrpfPrjkHCyb4CLQZD3nsQSYUKGH4PPsRS8J2M
     const privA = Buffer.from("92541d4cdb6f0db05949e4fd91d7cb936f84eb21a62b477103d0e1957e6ad782", "hex"); // prettier-ignore
     const pubkeyA = getPublicKey(privA, true);
     const pubkeyHashA = hash160(pubkeyA);
 
+    // address: myVmRHXHq6qzW7s7TCseBLLgajKxT6Z19j
+    // pkh:     c538c517797dfefdf30142dc1684bfd947532dbb
+    // wif:     cSAyZjTwLx2eh76MfL5zmGV8n7pk61Lx5We2S95UGyhiwoSgJgv2
     const privB = Buffer.from("88fb4d47adfd310bf071d67c8ac569f58d8e4c4d8d94b778c010ddd0e2ff6a48", "hex"); // prettier-ignore
     const pubkeyB = getPublicKey(privB, true);
     const pubkeyHashB = hash160(pubkeyB);
@@ -45,7 +24,7 @@ describe("TxBuilder", () => {
     /**
      * Spends a transaction with a p2pkh input that pays to a p2pkh outputs
      */
-    function spend_p2pkh_to_p2pkh(): TxBuilder {
+    function spend_p2pkh_to_p2pkh() {
         const txBuilder = new TxBuilder();
         txBuilder.version = 2;
         txBuilder.addInput(OutPoint.fromString("9d0e63ad73020a9fad0106b6727e31d36e3ab4b9a01451233926d4759569de68:0")); // prettier-ignore
@@ -61,7 +40,7 @@ describe("TxBuilder", () => {
     /**
      * Spends a transaction with a p2pk input that pays to a p2pk output
      */
-    function spend_p2pk_to_p2pk(): TxBuilder {
+    function spend_p2pk_to_p2pk() {
         const sut = new TxBuilder();
         sut.version = 2;
         sut.addInput(OutPoint.fromString("68ce1030a63bd7ff44a95f497d3535731cfa3e6b89eda5ce38eb37a6d527d0dc:0")); // prettier-ignore
@@ -98,6 +77,52 @@ describe("TxBuilder", () => {
         return sut;
     }
 
+    function spend_p2pkh_to_p2ms() {
+        const sut = new TxBuilder();
+        sut.version = 2;
+        sut.addInput(
+            OutPoint.fromString(
+                "997fd2dd17a5d5843edc23ab7f043130dfe737cf0e02336c75fe37c1eda51195:0",
+            ),
+        );
+        sut.addOutput(
+            Value.fromBitcoin(49.9999),
+            new Script(OpCode.OP_2, pubkeyA, pubkeyB, OpCode.OP_2, OpCode.OP_CHECKMULTISIG),
+        );
+
+        const commitScript0 = Script.p2pkhLock(pubkeyHashA);
+        const sig0 = sut.sign(privA, 0, commitScript0);
+        const scriptSig0 = Script.p2pkhUnlock(sig0, pubkeyA);
+        sut.setScriptSig(0, scriptSig0);
+
+        return sut;
+    }
+
+    function spend_p2ms_to_p2pkh() {
+        const sut = new TxBuilder();
+        sut.version = 2;
+        sut.addInput(
+            OutPoint.fromString(
+                "d6422b4b4c9ec2e8f5f6eaff948241c494397f7e6ca4a2ca2783e3ea3581e27f:0",
+            ),
+        );
+        sut.addOutput(Value.fromBitcoin(49.9998), Script.p2pkhLock(pubkeyHashB));
+
+        const commitScript0 = new Script(
+            OpCode.OP_2,
+            pubkeyA,
+            pubkeyB,
+            OpCode.OP_2,
+            OpCode.OP_CHECKMULTISIG,
+        );
+        const sig0_1 = sut.sign(privA, 0, commitScript0);
+        const sig0_2 = sut.sign(privB, 0, commitScript0);
+        const scriptSig0 = new Script(OpCode.OP_0, sig0_1, sig0_2);
+        sut.setScriptSig(0, scriptSig0);
+
+        return sut;
+    }
+
     /**
      * Creates a transaction with a single P2SH-P2PKH input that spends
      * to a single P2SH output.
@@ -106,7 +131,7 @@ describe("TxBuilder", () => {
      * vin[0]
      *      txid:
      */
-    function spend_p2sh_p2pkh_to_p2sh(): TxBuilder {
+    function spend_p2sh_p2pkh_to_p2sh() {
         const sut = new TxBuilder();
         sut.version = 2;
         // p2sh-p2pkh
@@ -131,7 +156,7 @@ describe("TxBuilder", () => {
         return sut;
     }
 
-    function spend_p2sh_to_p2sh(): TxBuilder {
+    function spend_p2sh_to_p2sh() {
         const sut = new TxBuilder();
         sut.version = 2;
         sut.addInput(
@@ -193,6 +218,20 @@ describe("TxBuilder", () => {
             const sut = spend_p2pkh_to_p2sh_p2pkh();
             expect(sut.serialize().toString("hex")).to.equal(
                 "0200000001d9f0f6a22a81b0ec0e40a97c65b36b15e02ee9abf0a3d848212f38ec3ebd5bca000000006a47304402205b1d47b4f0db4fe99b035a87649fbdfc3867e10950d5412b2875cd3774a052c2022037e3c70bfa8e237ed409e409bf6c8b0aa574e44297ed4fc868264ce5148d437a012102c13bf903d6147a7fec59b450e2e8a6c174c35a11a7675570d10bd05bc3597996ffffffff01f0ca052a0100000017a914a7b6b1cc86e6d0b8876040a46da5a346b8662db487ffffffff",
+            );
+        });
+
+        it("spend p2pkh to p2ms", () => {
+            const sut = spend_p2pkh_to_p2ms();
+            expect(sut.serialize().toString("hex")).to.equal(
+                "02000000019511a5edc137fe756c33020ecf37e7df3031047fab23dc3e84d5a517ddd27f99000000006b483045022100eca82b19d1954f6f24292b12dfc49379b536c65c32871309a81ec220750a07bd02205fdff7a71a4727e2fb17b7f089037054a5e6ef30c3ce12796c661ec690292443012102c13bf903d6147a7fec59b450e2e8a6c174c35a11a7675570d10bd05bc3597996ffffffff01f0ca052a0100000047522102c13bf903d6147a7fec59b450e2e8a6c174c35a11a7675570d10bd05bc3597996210334acee9adf0e3e490a422dfe98bc10a8091b43047b793b8d840657b6b6a46c5652aeffffffff",
+            );
+        });
+
+        it("spend p2ms to p2pkh", () => {
+            const sut = spend_p2ms_to_p2pkh();
+            expect(sut.serialize().toString("hex")).to.equal(
+                "02000000017fe28135eae38327caa2a46c7e7f3994c4418294ffeaf6f5e8c29e4c4b2b42d6000000009300483045022100d13069b4f0405313c34e42732b66e8633dea520cb39681f53872ba764d91694e02202b18904588344521a38f89fe2ca9273e3e0136a1bf96893f5ae6f50a5f6c4a1001483045022100e453e80486b0788c85b88c9be65740dcdb1de3c92781c828ccfa30f751bf5d75022056a890efe7b061f2986e2e542996f41324a180de80d03b947c009cb5cd1dd0d301ffffffff01e0a3052a010000001976a914c538c517797dfefdf30142dc1684bfd947532dbb88acffffffff",
             );
         });
 
