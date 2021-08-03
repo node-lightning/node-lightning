dd83c8ac785e33649194a8e85d0dd19d988d1889
Vib-UX <vibchess123@gmail.com>
2021-08-03 23:47:05 +0530

    Comments Update

--- packages/wire/lib/messages/ShutdownMessage.ts
+++ packages/wire/lib/messages/ShutdownMessage.ts
@@ -6,12 +6,12 @@ import { IWireMessage } from "./IWireMessage";
 /**
  * ShutdownMessage represents the `shutdown` message defined in
  * BOLT #2 of the Lightning Specification. This message can be sent by
- * either node. The scriptPubKey must be a valid P2WPKH, P2WSH, P2SH-P2WPKH, P2SH-P2WSH,
- * or any valid witness script if option_shutdown_anysegwit is negotiated which ensures the
- * resulting transaction will propagate to miners. If shutdown is sent by either node, corresponding
- * node should send commitment_signed to commit any outstanding changes before replying shutdown. Once
- * shutdown is sent by both nodes no new HTLCs should be added or accepted by the channel. After successful
- * handshake of shutdown message, fee negotiation and signature sending can begin with `closing_signed` message.
+ * either node. The scriptPubKey must be a valid P2WPKH, P2WSH, P2SH-P2WPKH, P2SH-P2WSH, or any
+ * valid witness script if option_shutdown_anysegwit is negotiated and it should be same as `shutdown_scriptpubkey`
+ * value if it was sent during `'open/accept' channel message`. Therefore, if both conditions hold true resulting transaction
+ * will propagate to miners. If shutdown is sent by either node, corresponding node should send commitment_signed to commit any
+ * outstanding changes before replying shutdown. Once shutdown is sent by both nodes no new HTLCs should be added or accepted by the channel.
+ * After successful handshake of shutdown message, fee negotiation and signature sending can begin with `closing_signed` message.
  */
 
 export class ShutdownMessage implements IWireMessage {
