a13a4efbc652529102128b6d55670a492c03a780
Vibchess123@gmail.com <vibchess123@gmail.com>
2021-08-16 20:31:41 +0530

    Null Check

--- packages/graph/lib/deserialize/channel-settings-from-message.ts
+++ packages/graph/lib/deserialize/channel-settings-from-message.ts
@@ -10,10 +10,12 @@ export function channelSettingsFromMessage(msg: ChannelUpdateMessage): ChannelSe
     instance.direction = msg.direction;
     instance.timestamp = msg.timestamp;
     instance.cltvExpiryDelta = msg.cltvExpiryDelta;
-    instance.htlcMinimumMsat = null;
-    instance.htlcMaximumMsat = null;
-    instance.feeBaseMsat = null;
-    instance.feeProportionalMillionths = null;
+    instance.htlcMinimumMsat = msg.htlcMinimumMsat ? msg.htlcMinimumMsat.msats : null;
+    instance.htlcMaximumMsat = msg.htlcMaximumMsat ? msg.htlcMaximumMsat.msats : null;
+    instance.feeBaseMsat = msg.feeBaseMsat ? Number(msg.feeBaseMsat.msats) : null;
+    instance.feeProportionalMillionths = msg.feeProportionalMillionths
+        ? Number(msg.feeProportionalMillionths.microsats)
+        : null;
     instance.disabled = msg.disabled;
     return instance;
 }
