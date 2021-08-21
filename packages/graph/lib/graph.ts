288d11f8d1e539bf3d1cdba571fbd5680283e2e4
Vib-UX <vibchess123@gmail.com>
2021-08-21 23:45:41 +0530

    Work in Progress

--- packages/graph/lib/graph.ts
+++ packages/graph/lib/graph.ts
@@ -7,6 +7,9 @@ import { Node } from "./node";
  * Graph represents a
  */
 export class Graph {
+    
+    public nodes_list: string[] = [];
+    public adjacencyList = {};
     /**
      * Map containing all nodes in the system
      */
@@ -27,10 +30,12 @@ export class Graph {
      */
     public addNode(node: Node) {
         this.nodes.set(node.nodeId.toString("hex"), node);
+        // Adding node_id's to the list
+        this.nodes_list.push(node.nodeId.toString("hex"));
     }
 
     /**
-     * Adds a channgel to the graph
+     * Adds a channel to the graph
      */
     public addChannel(channel: Channel) {
         const node1 = this.getNode(channel.nodeId1);
@@ -47,6 +52,10 @@ export class Graph {
 
         // attach channel to node 2
         node2.linkChannel(channel);
+        // Adjacency List is required to store all the channel links and it can be traversed using the node_id's
+        // The edge added b/w two channels is key to the channel, which can be later used to do computations accordingly
+        // when using dijkstra
+        this.adjacencyList[node1.nodeId.toString("hex")][node2.nodeId.toString("hex")] = key;
     }
 
     /**
@@ -79,5 +88,73 @@ export class Graph {
 
         // detach from node 2
         n2.unlinkChannel(channel);
+    }
+
+    public dijkstra(src: Node, dest: Node, amnt: bigint) {
+        // Convert it to corresponding node_id
+        const str_id = src.nodeId.toString("hex");
+
+        // Distances will store the base_fee required to traverse from the src
+        let distances = {},
+            // Parents will be used to find the path from dest to src later
+            parents = {},
+            visited = new Set();
+        // Initially we are setting up the dist value to be
+        for (let i = 0; i < this.nodes_list.length; i++) {
+            if (this.nodes_list[i] === str_id) {
+                distances[str_id] = 0;
+            } else {
+                distances[this.nodes_list[i]] = Infinity;
+            }
+            parents[this.nodes_list[i]] = null;
+        }
+
+        let currnode = this.nodeWithMinDistance(distances, visited);
+
+        while (currnode !== null) {
+            let distance = distances[currnode],
+                neighbors = this.adjacencyList[currnode];
+            for (let neighbor in neighbors) {
+                // lets get the channel key for each link
+                let sid = this.channels.get(neighbors[neighbor]);
+                let newDistance =
+                    distance + sid.node1Settings.feeBaseMsat;
+                // A check to see if the our side of channel node can transfer the amnt required
+                if(sid.node1Settings.htlcMaximumMsat<amnt || sid.node1Settings.htlcMinimumMsat>amnt) continue; 
+                if (distances[neighbor] > newDistance) {
+                    distances[neighbor] = newDistance;
+                    parents[neighbor] = currnode;
+                }
+            }
+            visited.add(currnode);
+            currnode = this.nodeWithMinDistance(distances, visited);
+        }
+        console.log(parents);
+        console.log(distances);
+        // Lets print the route
+        console.log(this.path_ret(str_id,dest.nodeId.toString("hex"),parents)); 
+    }
+
+    private path_ret(src: string, dest: string, parent: {}){
+        let sidRoute = [];
+        while(parent[dest]!=null){
+            sidRoute.push(this.channels.get(this.adjacencyList[parent[dest]][dest]).shortChannelId);
+            dest = parent[dest];
+        }
+
+        return sidRoute;
+    }
+
+    private nodeWithMinDistance(distances, visited) {
+        let minDistance = Infinity,
+            minVertex = null;
+        for (let vertex in distances) {
+            let distance = distances[vertex];
+            if (distance < minDistance && !visited.has(vertex)) {
+                minDistance = distance;
+                minVertex = vertex;
+            }
+        }
+        return minVertex;
     }
 }
