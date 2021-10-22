ab336cf85603669ca3c8389e3e9f2cdd383a4359
bmancini55 <bmancini@gmail.com>
2021-10-22 07:55:08 -0400

    bitcoin: add segwit address decoding vectors
    Implements additional checks in Address.decodeSegwit and adds test
    vectors from BIP350 using bech32m for segwit v1+.

--- packages/bitcoin/__tests__/Address.spec.ts
+++ packages/bitcoin/__tests__/Address.spec.ts
@@ -198,5 +198,106 @@ describe("Address", () => {
                 });
             }
         });
+
+        describe("BIP350 Vectors", () => {
+            const vectors = [
+                [
+                    "BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4",
+                    "0014751e76e8199196d454941c45d1b3a323f1433bd6",
+                ],
+                [
+                    "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
+                    "00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262",
+                ],
+                [
+                    "bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kt5nd6y",
+                    "5128751e76e8199196d454941c45d1b3a323f1433bd6751e76e8199196d454941c45d1b3a323f1433bd6",
+                ],
+                ["BC1SW50QGDZ25J", "6002751e"],
+                ["bc1zw508d6qejxtdg4y5r3zarvaryvaxxpcs", "5210751e76e8199196d454941c45d1b3a323"],
+                [
+                    "tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
+                    "0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433",
+                ],
+                [
+                    "tb1pqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesf3hn0c",
+                    "5120000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433",
+                ],
+                [
+                    "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0",
+                    "512079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+                ],
+            ];
+
+            for (const [address, scriptPubKey] of vectors) {
+                it(`${address} => ${scriptPubKey}`, () => {
+                    const decoded = Address.decodeSegwit(address);
+                    const script = new Script(Script.number(decoded.version), decoded.program);
+                    expect(script.serializeCmds().toString("hex")).to.equal(scriptPubKey);
+                });
+            }
+        });
+
+        describe("BIP350 Vectors (Invalid)", () => {
+            const vectors = [
+                [
+                    "tc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq5zuyut",
+                    "Invalid human-readable part",
+                ],
+                [
+                    "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqh2y7hd",
+                    "Invalid checksum (Bech32 instead of Bech32m)",
+                ],
+                [
+                    "tb1z0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqglt7rf",
+                    "Invalid checksum (Bech32 instead of Bech32m)",
+                ],
+                [
+                    "BC1S0XLXVLHEMJA6C4DQV22UAPCTQUPFHLXM9H8Z3K2E72Q4K9HCZ7VQ54WELL",
+                    "Invalid checksum (Bech32 instead of Bech32m)",
+                ],
+                [
+                    "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kemeawh",
+                    "Invalid checksum (Bech32m instead of Bech32)",
+                ],
+                [
+                    "tb1q0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq24jc47",
+                    "Invalid checksum (Bech32m instead of Bech32)",
+                ],
+                [
+                    "bc1p38j9r5y49hruaue7wxjce0updqjuyyx0kh56v8s25huc6995vvpql3jow4",
+                    "Invalid character in checksum",
+                ],
+                [
+                    "BC130XLXVLHEMJA6C4DQV22UAPCTQUPFHLXM9H8Z3K2E72Q4K9HCZ7VQ7ZWS8R",
+                    "Invalid witness version",
+                ],
+                ["bc1pw5dgrnzv", "Invalid program length (1 byte)"],
+                [
+                    "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7v8n0nx0muaewav253zgeav",
+                    "Invalid program length (41 bytes)",
+                ],
+                [
+                    "BC1QR508D6QEJXTDG4Y5R3ZARVARYV98GJ9P",
+                    "Invalid program length for witness version 0 (per BIP141)",
+                ],
+                ["tb1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq47Zagq", "Mixed case"],
+                [
+                    "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7v07qwwzcrf",
+                    "zero padding of more than 4 bits",
+                ],
+                [
+                    "tb1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vpggkg4j",
+                    "Non-zero padding in 8-to-5 conversion",
+                ],
+                ["bc1gmk9yu", "Empty data section"],
+            ];
+
+            for (const [address, error] of vectors) {
+                it(`${address} (${error})`, () => {
+                    expect(() => Address.decodeSegwit(address)).to.throw();
+                });
+            }
+        });
     });
 });
