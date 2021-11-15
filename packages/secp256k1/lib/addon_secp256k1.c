5cd8819282899e07a2dd6f2aeba4018a7b1e7c03
bmancini55 <bmancini@gmail.com>
2021-11-15 16:23:19 -0500

    secp256k1: initial project setup

---
+++ packages/secp256k1/lib/addon_secp256k1.c
@@ -0,0 +1,4 @@
+#include <addon_secp256k1.h>
+#include <libsecp256k1/include/secp256k1_ecdh.h>
+#include <libsecp256k1/include/secp256k1_preallocated.h>
+#include <libsecp256k1/include/secp256k1_recovery.h>
---
+++ packages/secp256k1/lib/addon_secp256k1.h
@@ -0,0 +1,2 @@
+#include <libsecp256k1/include/secp256k1.h>
+#include <node_api.h>
