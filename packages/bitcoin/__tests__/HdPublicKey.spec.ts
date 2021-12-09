c4acbc8c4458aba717f7ab20c7d69242553bbff1
bmancini55 <bmancini@gmail.com>
2021-12-09 12:46:37 -0500

    bitcoin: add tests for HdPublicKey

--- packages/bitcoin/__tests__/HdPublicKey.spec.ts
+++ packages/bitcoin/__tests__/HdPublicKey.spec.ts
@@ -1,9 +1,44 @@
 import { expect } from "chai";
-import { HdKeyType } from "../lib";
+import { HdKeyType, HdPublicKey } from "../lib";
 import { HdPrivateKey } from "../lib/HdPrivateKey";
 import { Network } from "../lib/Network";
 
 describe("HdPublicKey", () => {
+    const seed = Buffer.alloc(32, 0x01);
+
+    describe("#decode()", () => {
+        it("throws if given xprv", () => {
+            expect(() =>
+                HdPublicKey.decode(
+                    "xprv9uHRZZhk6KAJC1avXpDAp4MDc3sQKNxDiPvvkX8Br5ngLNv1TxvUxt4cV1rGL5hj6KCesnDYUhd7oWgT11eZG7XnxHrnYeSvkzY7d2bhkJ7",
+                ),
+            ).to.throw("Invalid HD public key");
+        });
+    });
+
+    describe(".isHardened", () => {
+        it("true when hardened", () => {
+            const account = HdPrivateKey.fromPath("m/84'/0'/0'", seed, Network.mainnet);
+            expect(account.toPubKey().isHardened).to.equal(true);
+        });
+
+        it("false when not-hardened", () => {
+            const account = HdPrivateKey.fromPath("m/84'/0'/0'", seed, Network.mainnet);
+            expect(account.derive(0).toPubKey().isHardened).to.equal(false);
+        });
+    });
+
+    describe(".fingerprint", () => {
+        const seed = Buffer.from("000102030405060708090a0b0c0d0e0f", "hex");
+        it("returns fingerprint", () => {
+            const master = HdPrivateKey.fromSeed(seed, Network.mainnet).toPubKey();
+            const expected = HdPublicKey.decode(
+                "xpub68Gmy5EdvgibQVfPdqkBBCHxA5htiqg55crXYuXoQRKfDBFA1WEjWgP6LHhwBZeNK1VTsfTFUHCdrfp1bgwQ9xv5ski8PX9rL2dZXvgGDnw",
+            ).parentFingerprint;
+            expect(master.fingerprint.slice(0, 4)).to.deep.equal(expected);
+        });
+    });
+
     describe(".derive()", () => {
         it("pub key can derive pub key", () => {
             const seed = Buffer.alloc(32, 0x01);
@@ -12,29 +47,6 @@ describe("HdPublicKey", () => {
             const key = pubkey.derive(0).derive(1);
             expect(key.depth).to.equal(2);
         });
-    });
-
-    describe(".toAddress()", () => {
-        const seed = Buffer.alloc(32, 0x01);
-
-        it("xpub", () => {
-            const prvkey = HdPrivateKey.fromPath("m/0", seed, Network.mainnet, HdKeyType.x);
-            expect(prvkey.toAddress()).to.equal("1J5RZmj33xPt9TRnHFEKDoZgxqT2e2us12");
-        });
-
-        it("ypub", () => {
-            const prvkey = HdPrivateKey.fromPath("m/0", seed, Network.mainnet, HdKeyType.y);
-            expect(prvkey.toAddress()).to.equal("3PmmbYZxjBnsyLaC3oehbHpZqciLbxiEog");
-        });
-
-        it("zpub", () => {
-            const prvkey = HdPrivateKey.fromPath("m/0", seed, Network.mainnet, HdKeyType.z);
-            expect(prvkey.toAddress()).to.equal("bc1qhdgyak30llluz36r6lk5ywup5uq0jz0ecsecfv");
-        });
-    });
-
-    describe(".derive()", () => {
-        const seed = Buffer.alloc(32, 0x01);
 
         it("matches HdPrivateKey derive", () => {
             const account = HdPrivateKey.fromPath("m/84'/0'/0'", seed, Network.mainnet);
@@ -49,6 +61,73 @@ describe("HdPublicKey", () => {
                 .derive(0)
                 .toSecHex();
             expect(actual).to.equal(expected);
+        });
+
+        it("throws with hardened", () => {
+            const account = HdPrivateKey.fromPath("m/84'/0'/0'", seed, Network.mainnet);
+            expect(() => account.toPubKey().derive(0x80000000)).to.throw(
+                "Invalid HD key derivation",
+            );
+        });
+    });
+
+    describe(".toAddress()", () => {
+        const seed = Buffer.alloc(32, 0x01);
+
+        it("xpub", () => {
+            const pubkey = HdPrivateKey.fromPath(
+                "m/0",
+                seed,
+                Network.mainnet,
+                HdKeyType.x,
+            ).toPubKey();
+            expect(pubkey.toAddress()).to.equal("1J5RZmj33xPt9TRnHFEKDoZgxqT2e2us12");
+        });
+
+        it("ypub", () => {
+            const pubkey = HdPrivateKey.fromPath(
+                "m/0",
+                seed,
+                Network.mainnet,
+                HdKeyType.y,
+            ).toPubKey();
+            expect(pubkey.toAddress()).to.equal("3PmmbYZxjBnsyLaC3oehbHpZqciLbxiEog");
+        });
+
+        it("zpub", () => {
+            const pubkey = HdPrivateKey.fromPath(
+                "m/0",
+                seed,
+                Network.mainnet,
+                HdKeyType.z,
+            ).toPubKey();
+            expect(pubkey.toAddress()).to.equal("bc1qhdgyak30llluz36r6lk5ywup5uq0jz0ecsecfv");
+        });
+    });
+
+    describe(".toSecBuffer()", () => {
+        it("returns underlying buffer", () => {
+            const pubkey = HdPrivateKey.fromPath(
+                "m/84'/0'/0'/0/0",
+                seed,
+                Network.mainnet,
+            ).toPubKey();
+            const actual = pubkey.toSecBuffer();
+            const expected = pubkey.publicKey.toBuffer();
+            expect(actual).to.deep.equal(expected);
+        });
+    });
+
+    describe(".toSecHex()", () => {
+        it("returns underlying buffer", () => {
+            const pubkey = HdPrivateKey.fromPath(
+                "m/84'/0'/0'/0/0",
+                seed,
+                Network.mainnet,
+            ).toPubKey();
+            const actual = pubkey.toSecHex();
+            const expected = pubkey.publicKey.toHex();
+            expect(actual).to.deep.equal(expected);
         });
     });
 });
