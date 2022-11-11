16ade84a46480f429352a0ca91533abeee3e6e4b
Brian Mancini <bmancini@gmail.com>
2022-11-11 16:54:44 -0500

    wire: update IGossipFilterChainClient for Tx

--- packages/wire/lib/gossip/IGossipFilterChainClient.ts
+++ packages/wire/lib/gossip/IGossipFilterChainClient.ts
@@ -1,19 +1,19 @@
 /**
  * The interface required by the GossipFilter to perform on-chain validation checks
  * of messages. A simplified interface is required by the GossipFilter. The actual
- * chain client implementation may offer a broader set of feautures. Additionally,
- * an adapter could be constructred to make an chain client conform to that required
+ * chain client implementation may offer a broader set of features. Additionally,
+ * an adapter could be constructed to make an chain client conform to that required
  * by the GossipFilter.
  */
 export interface IGossipFilterChainClient {
     getBlockHash(height: number): Promise<string>;
-    getBlock(hash: string): Promise<HasTxStrings>;
+    getBlock(hash: string): Promise<HasTx>;
     getUtxo(txId: string, voutIdx: number): Promise<HasScriptPubKey & HasValue>;
     waitForSync(): Promise<boolean>;
 }
 
-export type HasTxStrings = {
-    tx: string[];
+export type HasTx = {
+    tx: any[];
 };
 
 export type HasScriptPubKey = {
