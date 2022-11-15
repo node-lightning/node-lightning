15f287eaa421ad25bc107377e9afbe8a0bdd0232
Brian Mancini <bmancini@gmail.com>
2022-11-15 14:08:26 -0500

    core: lint fixes for AsyncProcessingQueue

--- packages/core/lib/AsyncProcessingQueue.ts
+++ packages/core/lib/AsyncProcessingQueue.ts
@@ -7,14 +7,13 @@ import { Queue } from "./Queue";
  */
 export class AsyncProcessingQueue<T> extends EventEmitter {
     private _fn: (...args: T[]) => Promise<void>;
-    private _queue: Queue<any>;
+    private _queue: Queue<T>;
     private _flushHandle: any;
 
     constructor(fn: (...args: T[]) => Promise<void>) {
         super();
         this._fn = fn;
         this._queue = new Queue();
-        this._flush = this._flush.bind(this);
     }
 
     /**
@@ -25,14 +24,14 @@ export class AsyncProcessingQueue<T> extends EventEmitter {
 
         // Postpone flushing until end of event loop to allow multiple operations
         // to enqueue. This handle will be cleared once flushing has completed.
-        if (!this._flushHandle) this._flushHandle = setImmediate(this._flush);
+        if (!this._flushHandle) this._flushHandle = setImmediate(() => void this._flush());
     }
 
     /**
      * Gets the number of pending items in the processor queue
      * @type {number}
      */
-    get size(): number {
+    public get size(): number {
         return this._queue.length;
     }
 
