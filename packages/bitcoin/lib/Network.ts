9c4405987549b0faca80e5785de4aa779396d279
Brian Mancini <bmancini@gmail.com>
2022-11-17 16:17:15 -0500

    bitcoin: add genesisHash property to the network
    Adds the genesis block hash to the network. This modifies the prebuilt
    networks and makes regtest into a factory function that accepts the
    block hash.

--- packages/bitcoin/lib/Network.ts
+++ packages/bitcoin/lib/Network.ts
@@ -1,6 +1,27 @@
+import { HashValue } from "./HashValue";
+
 export class Network {
+    public static regtest(genesisHash: HashValue): Network {
+        return new Network(
+            "regtest",
+            genesisHash,
+            0x6f, // m or n
+            0xc4, // 2
+            "bcrt",
+            "bcrt",
+            0x043587cf, // tpub
+            0x04358394, // tprv
+            0x044a5262, // upub
+            0x044a4e28, // uprv
+            0x045f1cf6, // vpub
+            0x045f18bc, // vprv
+            0xef,
+        );
+    }
+
     constructor(
         readonly name: string,
+        readonly genesisHash: HashValue,
         readonly p2pkhPrefix: number,
         readonly p2shPrefix: number,
         readonly p2wpkhPrefix: string,
@@ -26,10 +47,6 @@ export class Network {
         return testnet;
     }
 
-    public static get regtest(): Network {
-        return regtest;
-    }
-
     public static get all(): Network[] {
         return all;
     }
@@ -37,6 +54,7 @@ export class Network {
 
 const mainnet = new Network(
     "mainnet",
+    HashValue.fromRpc("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
     0x00, // 1
     0x05, // 3
     "bc",
@@ -52,6 +70,7 @@ const mainnet = new Network(
 
 const testnet = new Network(
     "testnet",
+    HashValue.fromRpc("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"),
     0x6f, // m or n
     0xc4, // 2
     "tb",
@@ -65,19 +84,4 @@ const testnet = new Network(
     0xef,
 );
 
-const regtest = new Network(
-    "regtest",
-    0x6f, // m or n
-    0xc4, // 2
-    "bcrt",
-    "bcrt",
-    0x043587cf, // tpub
-    0x04358394, // tprv
-    0x044a5262, // upub
-    0x044a4e28, // uprv
-    0x045f1cf6, // vpub
-    0x045f18bc, // vprv
-    0xef,
-);
-
-const all = [mainnet, testnet, regtest];
+const all = [mainnet, testnet];
