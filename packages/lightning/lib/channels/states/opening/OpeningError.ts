0499c36f680f128265050aad794043872893f6ee
Brian Mancini <bmancini@gmail.com>
2022-12-14 17:20:23 -0500

    channels: add errors types for validateAcceptChannel

--- packages/lightning/lib/channels/states/opening/OpeningError.ts
+++ packages/lightning/lib/channels/states/opening/OpeningError.ts
@@ -1,6 +1,11 @@
+import { Result } from "../../../Result";
 import { OpeningErrorType } from "./OpeningErrorType";
 
 export class OpeningError extends Error {
+    public static toResult<T>(type: OpeningErrorType): Result<T, OpeningError> {
+        return Result.err(new OpeningError(type));
+    }
+
     constructor(readonly type: OpeningErrorType) {
         super(OpeningErrorType[type]);
     }
