192c831bdbbc6aef8c042da084ae8f947df726ac
Brian Mancini <bmancini@gmail.com>
2023-01-06 11:13:41 -0500

    channels: implement AwaitingChannelReady.onChannelReadyMessage
    Implements transition 51

--- packages/lightning/lib/channels/states/opening/AwaitingChannelReadyState.ts
+++ packages/lightning/lib/channels/states/opening/AwaitingChannelReadyState.ts
@@ -1,3 +1,27 @@
+import { ChannelReadyMessage } from "../../../messages/ChannelReadyMessage";
+import { Channel } from "../../Channel";
 import { StateMachine } from "../../StateMachine";
+import { FailingState } from "../FailingState";
+import { NormalState } from "../NormalState";
 
-export class AwaitingChannelReadyState extends StateMachine {}
+export class AwaitingChannelReadyState extends StateMachine {
+    // eslint-disable-next-line @typescript-eslint/require-await
+    public async onChannelReadyMessage(
+        channel: Channel,
+        msg: ChannelReadyMessage,
+    ): Promise<string> {
+        // validate the message
+        const isOk = this.logic.validateChannelReadyMessage(channel, msg);
+
+        // fail if not ok which will transition to a local unilateral close
+        if (!isOk) {
+            return FailingState.name;
+        }
+
+        // attach the next_per_commitment_point
+        channel.attachChannelReady(msg);
+
+        // transition to normal operation
+        return NormalState.name;
+    }
+}
