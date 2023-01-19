14b109a99deebc290cdf67179b275191a973d8a4
Brian Mancini <bmancini@gmail.com>
2023-01-19 12:09:58 -0500

    channels: adjust ChannelSide test

--- packages/lightning/__tests__/channels/ChannelSide.spec.ts
+++ packages/lightning/__tests__/channels/ChannelSide.spec.ts
@@ -80,43 +80,34 @@ describe(ChannelSide.name, () => {
             const result = side.toJSON();
 
             // assert
-            expect(result.balance).to.equal(1);
-            expect(result.channelReserve).to.equal(0.1);
-            expect(result.commitmentNumber).to.equal(500_000n);
-            expect(result.commitmentPoint).to.equal(
-                "031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f",
-            );
-            expect(result.commitmentSig).to.equal(
-                "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-            );
-            expect(result.delayedBasePoint).to.equal(
-                "024d4b6cd1361032ca9bd2aeb9d900aa4d45d9ead80ac9423374c451a7254d0766",
-            );
-            expect(result.dustLimit).to.equal(0.0001);
-            expect(result.fundingPubKey).to.equal(
-                "02531fe6068134503d2723133227c867ac8fa6c83c537e9a44c3c5bdbdcb1fe337",
-            );
-            expect(result.htlcBasePoint).to.equal(
-                "03462779ad4aad39514614751a71085f2f10e1c7a593e4e030efb5b8721ce55b0b",
-            );
-            expect(result.htlcCounter).to.equal(0);
-            expect(result.maxAcceptedHtlc).to.equal(100);
-            expect(result.maxInFlightHtlcValue).to.equal(0.1);
-            expect(result.minHtlcValue).to.equal(0.001);
-            expect(result.nextCommitmentNumber).to.equal(1n);
-            expect(result.nextCommitmentPoint).to.equal(
-                "0362c0a046dacce86ddd0343c6d3c7c79c2208ba0d9c9cf24a6d046d21d21f90f7",
-            );
-            expect(result.nextCommitmentSig).to.equal(
-                "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101",
-            );
-            expect(result.paymentBasePoint).to.equal(
-                "03f006a18d5653c4edf5391ff23a61f03ff83d237e880ee61187fa9f379a028e0a",
-            );
-            expect(result.revocationBasePoint).to.equal(
-                "02989c0b76cb563971fdc9bef31ec06c3560f3249d6ee9e5d83c57625596e05f6f",
-            );
-            expect(result.toSelfDelayBlocks).to.equal(144);
+            expect(result).to.deep.equal({
+                balance: 1,
+                channelReserve: 0.1,
+                commitmentNumber: 500000n,
+                commitmentPoint:
+                    "031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f",
+                commitmentSig:
+                    "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+                delayedBasePoint:
+                    "024d4b6cd1361032ca9bd2aeb9d900aa4d45d9ead80ac9423374c451a7254d0766",
+                dustLimit: 0.0001,
+                fundingPubKey: "02531fe6068134503d2723133227c867ac8fa6c83c537e9a44c3c5bdbdcb1fe337",
+                htlcBasePoint: "03462779ad4aad39514614751a71085f2f10e1c7a593e4e030efb5b8721ce55b0b",
+                htlcCounter: 0,
+                maxAcceptedHtlc: 100,
+                maxInFlightHtlcValue: 0.1,
+                minHtlcValue: 0.001,
+                nextCommitmentNumber: 1n,
+                nextCommitmentPoint:
+                    "0362c0a046dacce86ddd0343c6d3c7c79c2208ba0d9c9cf24a6d046d21d21f90f7",
+                nextCommitmentSig:
+                    "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101",
+                paymentBasePoint:
+                    "03f006a18d5653c4edf5391ff23a61f03ff83d237e880ee61187fa9f379a028e0a",
+                revocationBasePoint:
+                    "02989c0b76cb563971fdc9bef31ec06c3560f3249d6ee9e5d83c57625596e05f6f",
+                toSelfDelayBlocks: 144,
+            });
         });
     });
 });
