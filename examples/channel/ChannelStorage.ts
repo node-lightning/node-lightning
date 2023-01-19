ac8e0287499e9514ffc85eb8e5da4a67bb8abe8f
Brian Mancini <bmancini@gmail.com>
2023-01-19 12:15:05 -0500

    example_channels: refactor ChannelStorage
    Refactors to use the Channel.toJSON method which while not ideal to
    embed de/ser is a common enough usage that it will make sense to keep in in the
    class.

--- examples/channel/ChannelStorage.ts
+++ examples/channel/ChannelStorage.ts
@@ -14,57 +14,10 @@ export class ChannelStorage implements IChannelStorage {
      * @param channel
      */
     public async save(channel: Channel): Promise<void> {
-        const json = {
-            channelId: channel.channelId.toHex(),
-            delayedBasePointSecret: channel.delayedBasePointSecret.toHex(),
-            feeRatePerKw: channel.feeRatePerKw.bitcoin,
-            funder: channel.funder,
-            fundingAmount: channel.fundingAmount.bitcoin,
-            fundingConfirmedHeight: channel.fundingConfirmedHeight,
-            fundingKey: channel.fundingKey.toHex(),
-            fundingOutPoint: channel.fundingOutPoint?.toString(),
-            fundingScript: channel.fundingScript?.toHex(),
-            fundingTx: channel.fundingTx?.toHex(),
-            htlcBasePointSecret: channel.htlcBasePointSecret.toHex(),
-            isPublic: channel.isPublic,
-            minimumDepth: channel.minimumDepth,
-            network: channel.network.name,
-            openBlockHeight: channel.openBlockHeight,
-            ourSide: this.channelSideToJson(channel.ourSide),
-            paymentBasePointSecret: channel.paymentBasePointSecret.toHex(),
-            peerId: channel.peerId,
-            perCommitmentSeed: channel.perCommitmentSeed.toString("hex"),
-            pushAmount: channel.pushAmount.bitcoin,
-            revocationBasePointSecret: channel.revocationBasePointSecret.toHex(),
-            state: channel.state.name,
-            temporaryId: channel.temporaryId.toString("hex"),
-            theirSide: this.channelSideToJson(channel.theirSide),
-        };
         const filename = `channel_${channel.channelId.toString()}.json`;
-        await fs.writeFile(path.join(this.folder, filename), JSON.stringify(json));
-    }
-
-    public channelSideToJson(side: ChannelSide) {
-        return {
-            balance: side.balance?.bitcoin,
-            channelReserve: side.channelReserve?.bitcoin,
-            commitmentNumber: side.commitmentNumber?.value,
-            commitmentPoint: side.commitmentPoint?.toHex(),
-            commitmentSig: side.commitmentSig?.toHex(),
-            delayedBasePoint: side.delayedBasePoint?.toHex(),
-            dustLimit: side.dustLimit?.bitcoin,
-            fundingPubKey: side.fundingPubKey?.toHex(),
-            htlcBasePoint: side.htlcBasePoint?.toHex(),
-            htlcCounter: side.htlcCounter,
-            maxAcceptedHtlc: side.maxAcceptedHtlc,
-            maxInFlightHtlcValue: side.maxInFlightHtlcValue?.bitcoin,
-            minHtlcValue: side.minHtlcValue?.bitcoin,
-            nextCommitmentNumber: side.nextCommitmentNumber?.value,
-            nextCommitmentPoint: side.nextCommitmentPoint?.toHex(),
-            nextCommitmentSig: side.nextCommitmentSig?.toHex(),
-            paymentBasePoint: side.paymentBasePoint.toHex(),
-            revocationBasePoint: side.revocationBasePoint.toHex(),
-            toSelfDelayBlocks: side.toSelfDelayBlocks,
-        };
+        await fs.writeFile(
+            path.join(this.folder, filename),
+            JSON.stringify(JSON.stringify(channel)),
+        );
     }
 }
