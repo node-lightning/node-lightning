8b904860d9a745f62e62b51ec0afc52b12824141
Brian Mancini <bmancini@gmail.com>
2023-02-07 11:59:29 -0500

    lightning: adding additional exports for events

--- packages/lightning/lib/index.ts
+++ packages/lightning/lib/index.ts
@@ -1,9 +1,17 @@
 // tslint:disable-next-line: variable-name
 
 export * from "./Constants";
+export * from "./IBlockProducer";
+export * from "./ILightningEventMuxer";
+export * from "./LightningEvent";
+export * from "./LightningEventMuxer";
+export * from "./LightningEventResult";
+export * from "./LightningEventType";
+export * from "./LightningEventQueue";
 export * from "./MessageFactory";
 export * from "./MessageType";
 export * from "./Peer";
+export * from "./PeerRepository";
 export * from "./PeerManager";
 export * from "./PeerServer";
 export * from "./PeerState";
@@ -17,12 +25,27 @@ export * from "./Base32";
 export * from "./BigIntUtils";
 export * from "./BitField";
 export * from "./IPausedReadable";
+
 export * from "./LinkedList";
 export * from "./LinkedListNode";
 
+export * from "./channels/ChannelKeys";
+export * from "./channels/CommitmentNumber";
+export * from "./channels/CommitmentSecret";
+export * from "./channels/CommitmentSecret";
+export * from "./channels/CommitmentSecretStore";
 export * from "./channels/IChannelLogic";
 export * from "./channels/IChannelStorage";
 export * from "./channels/IChannelWallet";
+export * from "./channels/IStateMachine";
+export * from "./channels/ChannelManager";
+export * from "./channels/ChannelPreferences";
+export * from "./channels/ChannelSide";
+export * from "./channels/Helpers";
+export * from "./channels/OpenChannelRequest";
+
+export * from "./channels/StateMachineFactory";
+export * from "./channels/StateMachine";
 
 export * from "./domain/Address";
 export * from "./domain/AddressIPv4";
@@ -31,11 +54,6 @@ export * from "./domain/AddressTor2";
 export * from "./domain/AddressTor3";
 export * from "./domain/AddressType";
 export * from "./domain/ChannelId";
-export * from "./channels/ChannelKeys";
-export * from "./channels/CommitmentNumber";
-export * from "./channels/CommitmentSecret";
-export * from "./channels/CommitmentSecret";
-export * from "./channels/CommitmentSecretStore";
 export * from "./domain/NetworkType";
 export * from "./domain/ShortChannelId";
 export * from "./domain/ShortChannelIdUtils";
